e931a86af02f2c32d83cf64dbe66d9a94a6a0704
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-09-27 17:37:20 -0700

    full create_AIRSISDataframes example

--- localExamples/process_AIRSIS_dump.R
+++ localExamples/process_AIRSIS_dump.R
@@ -20,7 +20,7 @@ loadSpatialData('NaturalEarthAdm1')
 
 # Set up logging and log to the console all DEBUG and higher logging statements
 logger.setup()
-logger.setLevel(INFO)
+logger.setLevel(DEBUG)
 
 # ebamFiles=c('APCD_ebam_bam.csv','ARB2.csv','mrpsa_ebam.csv','TCAPCD_ebam.csv','USFS_ebam_bam.csv'),
 # # HACK -- temporarily remove ARB2.csv
@@ -38,8 +38,9 @@ logger.setLevel(INFO)
 # mrpsa_ebam.csv
 
 dumpFiles <- c('~/Data/monitors/ARB2.csv',        # EBAM
-               '~/Data/monitors/TCAPCD_ebam.csv', # EBAM
                '~/Data/monitors/GBUAPCD.csv',     # EBAM
+               '~/Data/monitors/ODEQ.csv',        # EBAM
+               '~/Data/monitors/TCAPCD_ebam.csv', # EBAM
                '~/Data/monitors/USFS_ebam.csv',   # EBAM
                '~/Data/monitors/WASHOE.csv',      # EBAM
                '~/Data/monitors/mrpsa_ebam.csv',  # EBAM
@@ -65,14 +66,43 @@ for (filepath in dumpFiles) {
 
 airsisNew <- monitor_combine(monitorList)
 
+newData <- airsisNew$data
+newMeta <- airsisNew$meta
+
+# Add has_PM2.5 to match existing meta
+newMeta$has_PM2.5 <- TRUE
+
 # TODO:  read in airsisOLD meta, data
+oldData <- get(load('~/Data/EBAM_PM2.5_Latest.RData'))
+oldMeta <- get(load('~/Data/EBAM_SitesMetadata.RData'))
+
+if ( !'monitorType' %in% names(oldMeta) ) {
+  # Add 'monitortype' and 'has_pm2.5' columns in the proper order
+  oldMeta <- oldMeta[1:20]
+  oldMeta$monitorType <- 'EBAM'
+  oldMeta$has_PM2.5 <- TRUE
+}
 
 # TODO:  create empty data dataframe with appropriate time axis
 
+todayGMT <- as.POSIXct( lubridate::today(), tz="GMT")
+timeAxis <- seq(todayGMT - lubridate::ddays(9), todayGMT + lubridate::ddays(1), by='hours')
+data <- data.frame(datetime=timeAxis)
+
 # TODO:  join old data to new axis, join new data to new axis, subset any valid data
+data <- dplyr::left_join(data, oldData, by='datetime')
+data <- dplyr::left_join(data, newData, by='datetime')
+
+allMissingMask <- apply(data, 2, function(x) { all(is.na(x)) })
+data <- data[,!allMissingMask]
 
 # TODO:  join all meta, subset to only include sites with valid data
+meta <- dplyr::bind_rows(oldMeta, newMeta)
+rownames(meta) <- meta$monitorID
+meta <- meta[colnames(data)[-1],]
 
 # TODO:  save both, and combined ws_monitor object
+
+
 
 
