9de6df111d11d78231be51d91f437a530f8dc866
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-09-29 14:27:09 -0700

    experiments with DNR_pilot_project.R

--- localExamples/DNR_pilot_project.R
+++ localExamples/DNR_pilot_project.R
@@ -65,13 +65,19 @@ monitor_timeseriesPlot(KettleFalls_24, type='l', col=col_24, lwd=4, add=TRUE)
 title("Hourly PM2.5 -- Kettle Falls, Washington")
 legend('topleft', "24-hour average", col=col_24, lwd=4)
 
+monitor_timeseriesPlot(Usk, AQIStyle="24", shadedNight=TRUE)
+col_24 <- adjustcolor('purple', 0.5)
+monitor_timeseriesPlot(Usk_24, type='l', col=col_24, lwd=4, add=TRUE)
+title("Hourly PM2.5 -- Usk, Washington")
+legend('topleft', "24-hour average", col=col_24, lwd=4)
 
 # -----------------------------------------------------------------------------
 # BEGIN Work with "engineering" data
 
 # This could be rolled up into a separate "airsis_createQCDataframe()" or some such.
 ###fileString <- airsis_downloadData('USFS', '1031', 20160701, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
-###fileString <- airsis_downloadData('USFS', '1033', 20160701, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
+fileString <- airsis_downloadData('USFS', '1032', 20160701, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
+fileString <- airsis_downloadData('USFS', '1033', 20160701, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
 ###fileString <- airsis_downloadData('APCD', '1013', 20160701, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
 fileString <- airsis_downloadData('ARB2', '1023', 20160601, 20161231, baseUrl="http://xxxx.airsis.com/vision/common/CSVExport.aspx?")
 
@@ -115,9 +121,75 @@ raw_timeOfDaySpaghettiPlot(df, 'RHx', 'America/Los_angeles', highlightDates=high
 layout(1)
 
 
+# Prepare for openair
+timeRange <- range(df$datetime)
+timeAxis <- seq(timeRange[1], timeRange[2], by="hour")
+oaDF <- data.frame(datetime=timeAxis)
+oaDF <- dplyr::left_join(oaDF, df, by='datetime')
+oaDF <- oaDF[,c('datetime','W.S','W.D','ConcHr')]
+names(oaDF) <- c('date','ws','wd','pm25')
+oaDF$pm25 <- oaDF$pm25*1000
+
+oaDF <- openair::rollingMean(oaDF, pollutant='ws', width=3, new.name='ws_3')
+oaDF <- openair::rollingMean(oaDF, pollutant='wd', width=3, new.name='wd_3')
+oaDF <- openair::rollingMean(oaDF, pollutant='pm25', width=3, new.name='pm25_3')
+
+openair::windRose(oaDF)
+
+openair::pollutionRose(oaDF, 'pm25', angle=45)
+
+# Create cardinal direction factors from 3-hour rolling mean wind
+sector_wind <- oaDF$wd_3 + 22.5
+sector_code <- .bincode(sector_wind, seq(22.5, 382.5, 45))
+cardinalDirection <- factor(sector_code, labels=c('NE','E','SE','S','SW','W','NW','N'))
+N <- cardinalDirection == "N"
+NE <- cardinalDirection == "NE"
+E <- cardinalDirection == "E"
+SE <- cardinalDirection == "SE"
+S <- cardinalDirection == "S"
+SW <- cardinalDirection == "SW"
+W <- cardinalDirection == "W"
+NW <- cardinalDirection == "NW"
+
+arrow_N <- '\u2191'
+arrow_NE <- '\u2197'
+arrow_E <- '\u2192'
+arrow_SE <- '\u2198'
+arrow_S <- '\u2193'
+arrow_SW <- '\u2199'
+arrow_SW <- '\u2190'
+arrow_W <- '\u2196'
+
+# Plot smoothed pm2.5
+plot(oaDF$pm25_3 ~ oaDF$date, type='l')
+
+atext(oaDF$date[N], 150, arrow_N, srt=0)
+###text(oaDF$date[NE], 140, arrow_NE) # not found in font
+text(oaDF$date[NE], 150, labels=arrow_N, srt=-45) # srt assumes ccw=positive
+text(oaDF$date[NW], 150, labels=arrow_N, srt=45) # srt assumes ccw=positive
+text(oaDF$date[SE], 130, labels=arrow_S, srt=45) # srt assumes ccw=positive
+text(oaDF$date[SW], 130, labels=arrow_S, srt=-45) # srt assumes ccw=positive
+text(oaDF$date[S], 130, arrow_S, srt=0)
 
 
+# Different idea for background wind colors
 
+# > levels(cardinalDirection)
+# [1] "NE" "E"  "SE" "S"  "SW" "W"  "NW" "N" 
+colors <- rep('transparent',8)
+# Highlight NW
+colors[1] <- adjustcolor('red',0.3) # NE
+colors[2] <- adjustcolor('purple',0.1) # E
+colors[3] <- adjustcolor('blue',0.2) # SE
+colors[4] <- adjustcolor('blue',0.3) # S
+colors[5] <- adjustcolor('blue',0.2) # SW
+colors[6] <- adjustcolor('purple',0.1) # W
+colors[7] <- adjustcolor('red',0.2) # NW
+colors[8] <- adjustcolor('red',0.3) # N
+
+
+plot(oaDF$date, oaDF$pm25_3, type='l', xlim=as.POSIXct(c(lubridate::ymd("2016-09-09"),lubridate::ymd("2016-09-18"))))
+abline(v=oaDF$date, col=colors[cardinalDirection], lwd=5)
 
 # END engineering data
 # -----------------------------------------------------------------------------
