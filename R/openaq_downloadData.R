d7350a0c386e8add2e492516aee01e4b5716f5ef
wenhuy <wenhuy@uw.edu>
2016-10-26 17:21:27 -0700

    delete verbose-related codes

--- R/openaq_downloadData.R
+++ R/openaq_downloadData.R
@@ -1,11 +1,10 @@
 #' @keywords OpenAQ
 #' @export
 #' @title Download and Aggregate Multiple Daily Data Files from OpenAQ
-#' @param countryCode two-character country code (ISO 3166-1 alpha-2)
 #' @param parameter pollutant name
 #' @param startdate desired staring date (integer or character representing YYYYMMDD)
 #' @param days desired number of days of data to assemble
-#' @param verbose logical flag to generate verbose output
+#' @param countryCode two-character country code (ISO 3166-1 alpha-2)
 #' @param baseUrl base URL for archived daily data
 #' @description This function downloads data from the OpenAQ Data Backups page
 #' hosted on Amazon S3. All data obtained are then combined into a single dataframe
@@ -29,8 +28,8 @@
 #' df <- openaq_downloadData(20160901, days=7)
 #' }
 
-openaq_downloadData <- function(countryCode='US', parameter='pm25',
-                                startdate='', days=1, verbose=FALSE,
+openaq_downloadData <- function(parameter='pm25',
+                                startdate='', days=1, countryCode='US',
                                 baseUrl="http://openaq-data.s3.amazonaws.com/") {
   
   # OpenAQ Standard headers
@@ -47,9 +46,8 @@ openaq_downloadData <- function(countryCode='US', parameter='pm25',
   for (i in 1:days) {
     datetime <- startdate + lubridate::days(i-1)
     datestamp <- strftime(datetime,'%Y-%m-%d', tz='GMT')
-    if (verbose) {
-      cat(paste0('\nGetting data for ',datestamp,'...'))
-    }
+    # TODO: logger!
+    logger.debug("Working on datastamp %s", datestamp)
     url <- paste0(baseUrl,datestamp,'.csv')
     df <- suppressMessages( readr::read_csv(url, skip=1, col_names=col_names, col_types=col_types, progress=FALSE) )
     # Subset dataframe here so we don't grow too big if someone asks for many days
@@ -57,7 +55,7 @@ openaq_downloadData <- function(countryCode='US', parameter='pm25',
     if ( !is.null(parameter) ) df <- df[df$parameter == parameter,]
     dfList[[i]] <- df
   }
-  if (verbose) cat('\n')
+  # TODO: logger!
   
   # Combine all dataframes
   df <- dplyr::bind_rows(dfList)
