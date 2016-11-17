f11342d8bb2f25d74301f345534508f3bd883d52
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-11-17 11:22:28 -0800

    openaq tweaks

--- R/openaq_downloadData.R
+++ R/openaq_downloadData.R
@@ -10,8 +10,7 @@
 #' hosted on Amazon S3. All data obtained are then combined into a single dataframe
 #' and returned.
 #' 
-#' Available parameters are limited to:
-#' PM2.5, PM10, ozone, sulfur dioxide, nitrogen dioxide, carbon monoxide, and black carbon
+#' Available parameters include:
 #' \enumerate{
 #' \item{pm25}{ -- PM2.5}
 #' \item{pm10}{ -- PM10}
@@ -28,9 +27,20 @@
 #' df <- openaq_downloadData(20160901, days=7)
 #' }
 
-openaq_downloadData <- function(parameter='pm25',
-                                startdate='', days=1, countryCode='US',
+openaq_downloadData <- function(parameter=NULL,
+                                startdate='', days=1, countryCode=NULL,
                                 baseUrl="http://openaq-data.s3.amazonaws.com/") {
+  
+  # Sanity Check -- validate parameter
+  validParameters <- c('pm25','pm10','o3','so2','no2','co','bc')
+  if ( !is.null(parameter) ) {
+    if ( !parameter %in% validParameters ) {
+      logger.error("parameter '%s' is not in: %s", parameter, paste0(validParameters, collapse=", "))
+      stop(paste0("parameter '",parameter,"' is not in: ", paste0(validParameters, collapse=", ")))
+    }
+  }
+  
+  # TODO:  validate countrycode -- this requires MazamaSpatialUtils to provide a vector of countryCodes
   
   # OpenAQ Standard headers
   col_names <- c("location", "city", "country", "utc", "local", "parameter", "value", "unit", "latitude", "longitude", "attribution")
@@ -42,12 +52,13 @@ openaq_downloadData <- function(parameter='pm25',
   # Pre-allocate an empty list of the appropriate length (basic R performance idiom)
   dfList <- vector(mode="list", length=days)
   
+  logger.info('Downloading %d daily data files from OpenAQ...',days)
+  
   # Loop through days and store each datafame in the list
   for (i in 1:days) {
     datetime <- startdate + lubridate::days(i-1)
     datestamp <- strftime(datetime,'%Y-%m-%d', tz='GMT')
-    # TODO: logger!
-    logger.debug("Working on datastamp %s", datestamp)
+    logger.debug("Downloading data for %s", datestamp)
     url <- paste0(baseUrl,datestamp,'.csv')
     df <- suppressMessages( readr::read_csv(url, skip=1, col_names=col_names, col_types=col_types, progress=FALSE) )
     # Subset dataframe here so we don't grow too big if someone asks for many days
@@ -55,12 +66,15 @@ openaq_downloadData <- function(parameter='pm25',
     if ( !is.null(parameter) ) df <- df[df$parameter == parameter,]
     dfList[[i]] <- df
   }
-  # TODO: logger!
-  
+
   # Combine all dataframes
   df <- dplyr::bind_rows(dfList)
   
-  # Only return distinct rows (no duplicates)
-  return(dplyr::distinct(df))
+  # Remove any duplicate rows
+  df <- dplyr::distinct(df)
+  
+  logger.info('Downloaded %d rows of OpenAQ data', nrow(df))
+  
+  return(df)
   
 }
