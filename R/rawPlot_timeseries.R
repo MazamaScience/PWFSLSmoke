73b7cf761ead198343e9ea76bdd7111701fa83a7
Rex Thompson <Thompson@Rexs-MBP.domain>
2016-12-05 10:40:17 -0800

    Started documentation for raw_enhance

--- R/rawPlot_timeseries.R
+++ R/rawPlot_timeseries.R
@@ -1,3 +1,9 @@
+# # From monitor_timeseriesPlot(): Creates a time series plot of PM2.5 data from a
+# # ws_monitor object (see note below). Optional arguments color code by AQI index,
+# # add shading to indicate nighttime, and adjust the time display (GMT vs. local).
+# 
+# # ======
+# 
 # # Create a function that accepts an enhanced raw dataframe and creates a time series plot with the same
 # # features as monitor_timeseriesPlot().
 # 
@@ -10,33 +16,31 @@
 # 
 # # use the following to set up some dummy raw_enhanced data to play with
 # if (FALSE) {
-#   
+# 
 #   library(PWFSLSmoke)
 #   library(openair)
 #   setwd("~/Projects/PWFSLSmoke/")
-#   
+# 
 #   load("localData/airsis_rawList.RData")
-#   source('~/Projects/PWFSLSmoke/R/raw_enhanced.R', echo=FALSE)
-#   
+#   source('~/Projects/Mazama/PWFSLSmoke/R/raw_enhance.R', echo=FALSE)
+# 
 #   raw <-   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
-#   ebam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
+#   ebam_airsis <- raw_enhance(raw, rawSource = rawSource)
+# 
 #   raw <- airsis_rawList$Naches; rawSource <- "AIRSIS" #ESAM AIRSIS
-#   esam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
+#   esam_airsis <- raw_enhance(raw, rawSource = rawSource)
+# 
 #   raw <- airsis_rawList$Usk; rawSource <- "WRCC" #ESAM WRCC
-#   esam_wrcc <- raw_enhanced(raw, rawSource = rawSource)
-#   
+#   esam_wrcc <- raw_enhance(raw, rawSource = rawSource)
+# 
 #   rm(raw)
 #   rm(rawSource)
-#   
+# 
 # }
 # 
 # rawPlot_timeseries <- function(raw) {
-#   
-#   openair::pollutionRose(raw, ws="windSpeed", wd="windDir",pollutant=parameter, normalise=normalise, angle=15, ...)
-#   # NOTE: unclear why we need to pass in a wind speed argument since not used in pollution rose...?!?
-#   
+# 
+# 
 # }
 # 
 # 
@@ -44,22 +48,22 @@
 # # FROM DNR_UTILS.R
 # 
 # 
-# DNR_stoveWindPlot <- function(ws_monitor, raw, 
+# DNR_stoveWindPlot <- function(ws_monitor, raw,
 #                               tempVar='AT', humidityVar='RHx', windVar='W.S',
 #                               title='', tlim=NULL) {
-#   
+# 
 #   # Flusing Winds
 #   windBin <- .bincode(raw$W.S, c(-Inf,1,2,5,10,Inf))
 #   for (i in 5) { colors[i] <- adjustcolor('blue',(i-1)/5) }
-#   
+# 
 #   # Create fancy woodburning index
 #   stoveIndex <- woodStoveIndex(raw, tempVar, humidityVar, rollingWidth=12, rollingAlign='left')
 #   stoveSmoke <- ws_monitor$data[,2]
 #   stoveSmoke[stoveIndex < 0.5] <- NA
-#   
+# 
 #   monitor_timeseriesPlot(ws_monitor, type='l', shadedNight=FALSE)
 #   points(stoveSmoke ~ ws_monitor$data$datetime, pch=16, col='purple')
 #   abline(v=raw$datetime, col=colors[windBin])
 #   legend('topleft', legend=c("Woodstove Index", "0-1 m/s Wind", "1-2 m/s Wind", "2-5 m/s Wind"), fill=c('purple',colors[1],colors[2],colors[3]))
-#   
+# 
 # }
