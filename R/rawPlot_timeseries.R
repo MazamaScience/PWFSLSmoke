54fb02a12140bdaa636bd9e82a87a302e6124535
Rex Thompson <Thompson@Rexs-MacBook-Pro.local>
2016-12-13 18:21:43 -0800

    Added features to rawPlot_timeseries

--- R/rawPlot_timeseries.R
+++ R/rawPlot_timeseries.R
@@ -21,72 +21,100 @@
 # # OUTLINE ===============
 # 
 # # function call takes in raw_enhance object (df), plus additional argument (as described below/above)
-# # 
+# #
 # # create plot of selected parameters
 # 
 # # ACTUAL CODE ==================
 # 
-# # use the following to set up some dummy raw_enhanced data to play with
+# # use the following to set up some dummy raw_enhanced data to play with.  Comment out before adding to package.
 # if (FALSE) {
-#   
+# 
 #   library(PWFSLSmoke)
 #   library(openair)
-#   setwd("~/Projects/PWFSLSmoke/")
-#   
+#   setwd("~/Projects/Mazama/PWFSLSmoke/")
+# 
 #   load("localData/airsis_rawList.RData")
 #   source('~/Projects/PWFSLSmoke/R/raw_enhance.R', echo=FALSE)
-#   
-#   raw <-   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
+# 
+#   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
 #   ebam_airsis <- raw_enhance(raw, rawSource = rawSource)
-#   
+# 
 #   raw <- airsis_rawList$Naches; rawSource <- "AIRSIS" #ESAM AIRSIS
 #   esam_airsis <- raw_enhance(raw, rawSource = rawSource)
-#   
+# 
 #   raw <- airsis_rawList$Usk; rawSource <- "WRCC" #ESAM WRCC
 #   esam_wrcc <- raw_enhance(raw, rawSource = rawSource)
-#   
+# 
 #   rm(raw)
 #   rm(rawSource)
-#   
+# 
 # }
 # 
 # 
 # # and here's the actual function
 # 
-# rawPlot_timeseries <- function(df, parameter="pm25", AQIStyle='', add=FALSE, ...) {
+# # Need the following to run:
+# # MazamaSpatialUtils
+# # lubridate
+# # 
+# 
+# rawPlot_timeseries <- function(df, parameter="pm25", AQIStyle='', useGMT=FALSE, shadedNight=FALSE, add=FALSE, type="l", ...) {
+# 
+#   # Formatting
+#   xlab <- "Date and Time (local)" #overwritten to say "GMT" at end if useGMT is true -- see time section below
 #   
-#   param <- df[[parameter]]
+#   #TODO: enable AQI colors if parameter == "pm25"
+#   #TODO: include warning if AQI style selected but parameter != "pm25"
 #   
-#   plot(rawE$datetime,param,
-#        xlab="Date and Time",
-#        ylab=parameter)
+#   # Chart Labels
+#   if (parameter == "airTemp") {
+#     ylab <- "Air Temperature (Deg C)"
+#   } else if (parameter == "relHum") {
+#     ylab <- "Relative Humidity (%)"
+#   } else if (parameter == "windSpeed") {
+#     ylab <- "Wind Speed (m/s)"
+#   } else if (parameter == "windDir") {
+#     ylab <- "Wind Direction (degrees)"
+#   } else if (parameter == "pm25") {
+#     ylab <- "PM2.5 (ug/m3)"
+#   } else if (parameter == "barPress") {
+#     ylab <- "Barometric Pressure (hPa)"
+#   }
 #   
+#   # MISC
+#   lon <- df$longitude[1]
+#   lat <- df$latitude[1]
 #   
+#   # TIME ===========
 #   
+#   if (useGMT) {
+#     datetime <- df[["datetime"]]
+#     xlab <- "Date and Time (GMT)"
+#   } else {
+#     timezone <- MazamaSpatialUtils::getTimezone(lon, lat, countryCodes=NULL, useBuffering=TRUE) #modified from addMazamaMetadata.R
+#     datetime <- lubridate::with_tz(df$datetime, tzone=timezone)
+#   }
+# 
+#   # PLOT ============
+#   
+#   param <- df[[parameter]] #used in Plot function below for main item to be plotted
+#   
+#   plot(datetime,param,
+#        xlab=xlab,
+#        ylab=ylab,
+#        type=type,
+#        ...)
+# 
+#   # shade nighttime hours 
+#   ###  if(nrow(meta) == 1 & !useGMT & shadedNight) {
+#   if ( shadedNight && !useGMT ) {
+#     timeInfo <- PWFSLSmoke::timeInfo(datetime, lon, lat, timezone)
+#     PWFSLSmoke::addShadedNights(timeInfo)
+#   }
+#   
+#   #TODO: add shadedBackgroun in the same manner as above
 #   
 # }
 # 
 # 
 # 
-# # FROM DNR_UTILS.R
-# 
-# 
-# DNR_stoveWindPlot <- function(ws_monitor, raw,
-#                               tempVar='AT', humidityVar='RHx', windVar='W.S',
-#                               title='', tlim=NULL) {
-#   
-#   # Flusing Winds
-#   windBin <- .bincode(raw$W.S, c(-Inf,1,2,5,10,Inf))
-#   for (i in 5) { colors[i] <- adjustcolor('blue',(i-1)/5) }
-#   
-#   # Create fancy woodburning index
-#   stoveIndex <- woodStoveIndex(raw, tempVar, humidityVar, rollingWidth=12, rollingAlign='left')
-#   stoveSmoke <- ws_monitor$data[,2]
-#   stoveSmoke[stoveIndex < 0.5] <- NA
-#   
-#   monitor_timeseriesPlot(ws_monitor, type='l', shadedNight=FALSE)
-#   points(stoveSmoke ~ ws_monitor$data$datetime, pch=16, col='purple')
-#   abline(v=raw$datetime, col=colors[windBin])
-#   legend('topleft', legend=c("Woodstove Index", "0-1 m/s Wind", "1-2 m/s Wind", "2-5 m/s Wind"), fill=c('purple',colors[1],colors[2],colors[3]))
-#   
-# }
