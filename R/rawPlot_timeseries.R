37cb73917cf1f5bd0f13ca6627fece5dff6c3086
Rex Thompson <rexs.thompson@gmail.com>
2016-12-15 15:11:51 -0800

    raw_enhance timezone and missing data periods

--- R/rawPlot_timeseries.R
+++ R/rawPlot_timeseries.R
@@ -23,6 +23,11 @@
 # # function call takes in raw_enhance object (df), plus additional argument (as described below/above)
 # #
 # # create plot of selected parameters
+# # 
+# # Need the following to run:
+# # MazamaSpatialUtils
+# # lubridate
+# #
 # 
 # # ACTUAL CODE ==================
 # 
@@ -31,7 +36,7 @@
 # 
 #   library(PWFSLSmoke)
 #   library(openair)
-#   setwd("~/Projects/Mazama/PWFSLSmoke/")
+#   setwd("~/Projects/PWFSLSmoke/")
 # 
 #   load("localData/airsis_rawList.RData")
 #   source('~/Projects/PWFSLSmoke/R/raw_enhance.R', echo=FALSE)
@@ -47,45 +52,50 @@
 # 
 #   rm(raw)
 #   rm(rawSource)
-# 
+#   
 # }
-# 
 # 
 # # and here's the actual function
 # 
-# # Need the following to run:
-# # MazamaSpatialUtils
-# # lubridate
-# # 
+# rawPlot_timeseries <- function(df, parameter="pm25", useGMT=FALSE, shadedNight=FALSE, add=FALSE, ...) {
 # 
-# rawPlot_timeseries <- function(df, parameter="pm25", AQIStyle='', useGMT=FALSE, shadedNight=FALSE, add=FALSE, type="l", ...) {
+#   args <- list(...)
+#   type <- args[['type']]
 # 
-#   # Formatting
-#   xlab <- "Date and Time (local)" #overwritten to say "GMT" at end if useGMT is true -- see time section below
+#   print(paste("type passed in via dots:",type))
+# 
+#   if(is.null(type)) {type <- "l"; typeSpec <- FALSE} else {typeSpec <- TRUE}
 #   
+#   # General Formatting
+#   xlab <- "Date and Time (local)" #overwritten to say "GMT" at end if useGMT is true -- see time section below
+# 
 #   #TODO: enable AQI colors if parameter == "pm25"
 #   #TODO: include warning if AQI style selected but parameter != "pm25"
-#   
-#   # Chart Labels
-#   if (parameter == "airTemp") {
+# 
+#   # Parameter-specific formatting
+#   if (parameter == "temperature") {
 #     ylab <- "Air Temperature (Deg C)"
-#   } else if (parameter == "relHum") {
+#   } else if (parameter == "humidity") {
 #     ylab <- "Relative Humidity (%)"
 #   } else if (parameter == "windSpeed") {
 #     ylab <- "Wind Speed (m/s)"
 #   } else if (parameter == "windDir") {
 #     ylab <- "Wind Direction (degrees)"
+#     if (!typeSpec) {type <- "p"} # change from line graph to dots unless specifically requested to plot as line
 #   } else if (parameter == "pm25") {
 #     ylab <- "PM2.5 (ug/m3)"
-#   } else if (parameter == "barPress") {
+#   } else if (parameter == "pressure") {
 #     ylab <- "Barometric Pressure (hPa)"
 #   }
-#   
+# 
 #   # MISC
 #   lon <- df$longitude[1]
 #   lat <- df$latitude[1]
-#   
+# 
 #   # TIME ===========
+# 
+#   # NOTE: Moved timezome assignment to the raw_enhance function.
+#   # NOTE: May still need to check for monitors that have >1 deployment -- see deploymentID field
 #   
 #   if (useGMT) {
 #     datetime <- df[["datetime"]]
@@ -94,26 +104,33 @@
 #     timezone <- MazamaSpatialUtils::getTimezone(lon, lat, countryCodes=NULL, useBuffering=TRUE) #modified from addMazamaMetadata.R
 #     datetime <- lubridate::with_tz(df$datetime, tzone=timezone)
 #   }
-# 
+#   
 #   # PLOT ============
 #   
-#   param <- df[[parameter]] #used in Plot function below for main item to be plotted
+#   print(paste("Before plotting, type =",type))
 #   
+#   # # need to merge type back into the ... argument for plot function...
+#   args[['type']] <- type
+#   
+#   print(paste("args:",args))
+#   #do.call(identical, args)
+#   
+#   param <- df[[parameter]] #used in Plot function below for main item to be plotted
+# 
 #   plot(datetime,param,
 #        xlab=xlab,
 #        ylab=ylab,
-#        type=type,
-#        ...)
+#        ...=args)
 # 
-#   # shade nighttime hours 
+#   # shade nighttime hours
 #   ###  if(nrow(meta) == 1 & !useGMT & shadedNight) {
 #   if ( shadedNight && !useGMT ) {
 #     timeInfo <- PWFSLSmoke::timeInfo(datetime, lon, lat, timezone)
 #     PWFSLSmoke::addShadedNights(timeInfo)
 #   }
-#   
-#   #TODO: add shadedBackgroun in the same manner as above
-#   
+# 
+#   #TODO: add shadedBackground in the same manner as above
+# 
 # }
 # 
 # 
--- R/raw_enhance.R
+++ R/raw_enhance.R
@@ -1,10 +1,12 @@
+# 
 # #' @keywords raw_enhance
 # #' @export
-# #' @importFrom utils download.file
-# #' @title Process raw monitoring data to create raw_enhanced object
+# #' @title Process Raw Monitoring Data to Create raw_enhance Object
 # #' @param raw raw monitor data, as created by airsis_createRawDataframe or wrcc_createRawDataframe
 # #' @param rawSource source of raw monitor data (i.e. "AIRSIS" or "WRCC")
-# #' @description Processes raw monitor data to create a consistent format that can be handled by various raw* functions. All original raw data is retained, and additional columns are added with names add consistently-named columns enabling processing from various sources.
+# #' @description Processes raw monitor data to create a consistent format that can be handled by various raw* functions.
+# #' All original raw data is retained, and additional columns are added with names add consistently-named columns enabling
+# #' processing from various sources.
 # #' @return Dataframe with original raw data, plus new columns with raw naming scheme for downstream use.
 # 
 # # ==============================
@@ -15,8 +17,22 @@
 # # add new columns to dataframe with select columns and data formatted to consistent standard
 # # return new dataframe
 # 
+# # NOTE:  should add check to ensure that data matches the type passed in (e.g. EBAM_AIRSIS); check col names
+# # NOTE:  If names don't match expected, we'll get NULL values for an entire field, which isn't ideal.
+# 
+# # NOTE: Future work and other ideas:
+# 
+# # could possibly pull out last six columns (meta data) into a $meta field like the ws_monitor object.
+# # Could call it raw_monitor (as opposed to ws_monitor). Only difference is that there is more than one
+# # column of data for each monitor, so this type would not be as amendable to combining with other monitors
+# # in the same way as the ws_monitor object.
+# 
+# # NOTE:  There is other data available for most (but not all monitors) that could be included, if we are
+# # NOTE:  OK with some monitor types always having NULLs in certain fields.
+# 
 # # TEMP BACKGROUND FILES FOR TESTING, ETC =================
 # 
+# # NOTE: Leave the following commented out when pushed to package, but leave here for quick reference, e.g. troubleshooting
 # if ( FALSE ) {
 # 
 #   library(PWFSLSmoke)
@@ -41,76 +57,78 @@
 # 
 # # FUNCTION ================================
 # 
-# raw_enhance <- function(raw, rawSource="AIRSIS") {
+# raw_enhance <- function(df, rawSource="AIRSIS") {
 # 
-#   # dataframe is in hand; let's go about identifying the type
+#   # Identify monitor type and source and save to variable
 #   monType <- paste0(raw$monitorType[1],"_",rawSource,sep="")
 # 
 #   df <- raw
 # 
-#   # TODO:  NEED TO CHECK UNITS OF MEASURE!!!!
-#   # NOTE:  should add check to ensure that data matches the type passed in (e.g. EBAM_AIRSIS); check col names
-#   # NOTE:  If names don't match expected, we'll get NULL values for an entire field, which isn't ideal.
-# 
 #   # NOTE:  Could improve logic below to automate a determination as to what kind of data we're working with...
 #   # NOTE:  doing so would eliminate the need to pass in the source; instead just determine source based on type and fields
 # 
+#   # CREATE NEW FIELDS ==================
+#   # TODO:  NEED TO CHECK UNITS OF MEASURE!!!!
 #   if ( monType=="EBAM_AIRSIS" ) {
 # 
 #     # TODO:  add line here to check for data consistency
 # 
-#     df$airTemp <- raw$AT
-#     df$relHum <- raw$RHx
+#     df$temperature <- raw$AT
+#     df$humidity <- raw$RHx
 #     df$windSpeed <- raw$W.S
 #     df$windDir <- raw$W.D
 #     df$pm25 <- raw$COncRT # TODO:  Need to review this field for appropriateness, and units
 #     df$longitude <- raw$medoidLon
 #     df$latitude <- raw$medoidLat
-#     df$barPress <- as.numeric(NA)
+#     df$pressure <- as.numeric(NA)
 #     df$dataSource <- "AIRSIS"
 # 
 #   } else if ( monType=="ESAM_AIRSIS" ) {
 # 
 #     # TODO:  add line here to check for data consistency
 # 
-#     df$airTemp <- raw$AT.C.
-#     df$relHum <- raw$RHx...
+#     df$temperature <- raw$AT.C.
+#     df$humidity <- raw$RHx...
 #     df$windSpeed <- raw$WS.M.S.
 #     df$windDir <- raw$WD.Deg.
 #     df$pm25 <- raw$Conc.mg.m3. # TODO:  Need to review this field for appropriateness, and units
 #     df$longitude <- raw$medoidLon
 #     df$latitude <- raw$medoidLat
-#     df$barPress <- (raw$BP.PA.)/100
+#     df$pressure <- (raw$BP.PA.)/100
 #     df$dataSource <- "AIRSIS"
 # 
 #   } else if ( monType=="ESAM_WRCC" ) {
 # 
 #     # TODO:  add line here to check for data consistency
 # 
-#     df$airTemp <- raw$AvAirTemp
-#     df$relHum <- raw$RelHumidity
+#     df$temperature <- raw$AvAirTemp
+#     df$humidity <- raw$RelHumidity
 #     df$windSpeed <- raw$WindSpeed
 #     df$windDir <- raw$WindDir
 #     df$pm25 <- raw$ConcRT # TODO:  Need to review this field for appropriateness, and units
 #     df$longitude <- raw$medoidLon
 #     df$latitude <- raw$medoidLat
-#     df$barPress <- raw$BaromPress
+#     df$pressure <- raw$BaromPress
 #     df$dataSource <- "WRCC"
 # 
 #   } else {
 # 
-#     print("Warning - Type not supported")
+#     stop(paste0("Monitor type (",monType,") not currently supported.",sep=""))
 # 
 #   }
 # 
-#   # could possibly pull out last six columns (meta data) into a $meta field like the ws_monitor object.
-#   # Could call it raw_monitor (as opposed to ws_monitor). Only difference is that there is more than one
-#   # column of data for each monitor, so this type would not be as amendable to combining with other monitors
-#   # in the same way as the ws_monitor object.
-# 
-#   # NOTE:  There is other data available for most (but not all monitors) that could be included, if we are
-#   # NOTE:  OK with some monitor types always having NULLS in certain fields.
-# 
+#   # TODO: Fill in missing periods with nulls!
+#   
+#     
+#   # TODO: Assign timezones here! Should only do this for each unique pair of lat/lons. Or use deployment ID.
+#   # NOTE: use !duplicated, e.g. raw$Latitude[!duplicated(raw$deploymentID)]
+#   
+#   df$timezone <- "N/A"
+#   for (i in unique(df$deploymentID)) {
+#     index <- which(df$deploymentID==i)
+#     df$timezone[index] <- MazamaSpatialUtils::getTimezone(df$longitude[index[1]], df$latitude[index[1]], countryCodes=NULL, useBuffering=TRUE) #modified from addMazamaMetadata.R
+#   }
+#   
 #   return(df)
 # 
 # }
