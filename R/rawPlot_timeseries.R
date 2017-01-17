21d8c703f9f2fa35045d72e115c2a7fd0473ea78
Rex Thompson <RexS.Thompson@gmail.com>
2017-01-17 12:40:26 -0800

    uncommented raw_enhance, and other tweaks for rollout

--- R/rawPlot_timeseries.R
+++ R/rawPlot_timeseries.R
@@ -15,32 +15,32 @@
 # ===== ACTUAL STUFF ==========================
 
 ###############################################################################
-# 
-# This function creates a plot of raw monitoring data, with various bells and 
+#
+# This function creates a plot of raw monitoring data, with various bells and
 # whistles per user input.
 #
 ###############################################################################
-# 
-# #' @keywords raw_enhance
-# #' @export
-# #' @title Plot Raw Monitoring Data
-# #' @param df raw_enhance monitor object, as created by raw_enhance.R
-# #' @param parameter OPTIONAL parameter to plot; defaults to "pm25". Other options include
-# #' c("temperature","humidity","windSpeed","windDir", pressure), or any of the other raw
-# #' parameters (do "names(df)" to see list of options)
-# #' @param useGMT Plot w/ time axis in GMT, as opposed to local time. This is default if >1 time zone in data.
-# #' @param shadedNight Shade background based on approximate sunrise/sunset times. Unavailable if >1 time zone in data.
-# #' Also note that for multiple deployments, this defaults to use the lat/lon for the first deployment, which in theory
-# #' could be somewhat unrepresentative, such as if deployments have a large range in latitude.
-# #' @param shadedBackground Add vertical lines corresponding to wind speed, binned into quartiles. Future iterations
-# #' may include options to choose which parameter to plot, which color to use, which intervals, etc.
-# #' @param add (not currently supported) Option to allow overplotting on existing plot landscape
-# #' @param tlim A vector that subsets the raw dataframe by time limits, e.g. c(20160915,20161010)
-# #' @param type Line type; defaults to best option for each type, unless specified otherwise in this argument.
-# #' @description Creates a plot of raw monitoring data as generated using raw_enhance.R.
-# 
-# # NOTE:  This next section has to be commented out when you build the package
-# # NOTE:  but it's great to keep around for debugging
+
+#' @keywords raw_enhance
+#' @export
+#' @title Plot Raw Monitoring Data
+#' @param df raw_enhance monitor object, as created by raw_enhance.R
+#' @param parameter OPTIONAL parameter to plot; defaults to "pm25". Other options include
+#' c("temperature","humidity","windSpeed","windDir", pressure), or any of the other raw
+#' parameters (do "names(df)" to see list of options)
+#' @param useGMT Plot w/ time axis in GMT, as opposed to local time. This is default if >1 time zone in data.
+#' @param shadedNight Shade background based on approximate sunrise/sunset times. Unavailable if >1 time zone in data.
+#' Also note that for multiple deployments, this defaults to use the lat/lon for the first deployment, which in theory
+#' could be somewhat unrepresentative, such as if deployments have a large range in latitude.
+#' @param shadedBackground Add vertical lines corresponding to wind speed, binned into quartiles. Future iterations
+#' may include options to choose which parameter to plot, which color to use, which intervals, etc.
+#' @param add (not currently supported) Option to allow overplotting on existing plot landscape
+#' @param tlim A vector that subsets the raw dataframe by time limits, e.g. c(20160915,20161010)
+#' @param type Line type; defaults to best option for each type, unless specified otherwise in this argument.
+#' @description Creates a plot of raw monitoring data as generated using raw_enhance.R.
+
+# NOTE:  This next section has to be commented out when you build the package
+# NOTE:  but it's great to keep around for debugging
 # 
 # if (FALSE) {
 # 
@@ -52,7 +52,7 @@
 #   source('~/Projects/PWFSLSmoke/R/raw_enhance.R', echo=FALSE)
 # 
 #   rawEnhanceList <- list()
-#   
+# 
 #   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
 #   rawEnhanceList$EBAM_AIRSIS <- raw_enhance(raw, rawSource = rawSource)
 # 
@@ -65,138 +65,138 @@
 #   rm(raw)
 #   rm(rawSource)
 #   rm(airsis_rawList)
-#   
+# 
 #   lapply(rawEnhanceList,head)
 # 
 # }
-# 
-# rawPlot_timeseries <- function(df,
-#                                parameter="pm25",
-#                                useGMT=FALSE,
-#                                shadedNight=FALSE,
-#                                shadedBackground=NULL, #specify parameter to shade
-#                                #add=FALSE,
-#                                tlim=NULL,
-#                                type=NULL,
-#                                linelwd=4,
-#                                sblwd=1,
-#                                ...) {
-# 
-#   # If passed as argument, check that parameter exists in names of df
-#   if (!(parameter %in% names(df))) {
-#     stop(paste0("'",parameter,"' does not exist in names(",deparse(substitute(df)),")",sep=""))
-#   }
-# 
-#   # ----- Style ---------------------------------------------------------------
-# 
-#   # Time axis labels
-#   xlabLocal <- "Date and Time (local)" # used if GMT==FALSE
-#   xlabGMT <- "Date and Time (GMT)" # used if GMT==TRUE, or if time data is in >1 timezone
-# 
-#   # Line type default (can be overwritten)
-#   if(is.null(type)) {type <- "l"; typeSpec <- FALSE} else {typeSpec <- TRUE}
-# 
-#   # Parameter-specific styling
-#   if (parameter == "temperature") {
-#     ylab <- "Air Temperature (Deg C)"
-#     title <- "Air Temperature"
-#   } else if (parameter == "humidity") {
-#     ylab <- "Relative Humidity (%)"
-#     title <- "Relative Humidity"
-#   } else if (parameter == "windSpeed") {
-#     ylab <- "Wind Speed (m/s)"
-#     title <- "Wind Speed"
-#   } else if (parameter == "windDir") {
-#     ylab <- "Wind Direction (degrees)"
-#     if (!typeSpec) {type <- "p"} # change from line graph to dots unless specifically requested to plot as line
-#     title <- "Wind Direction"
-#   } else if (parameter == "pm25") {
-#     ylab <- expression(paste("PM"[2.5]*" (",mu,"g/m"^3*")"))
-#     title <- expression("PM"[2.5]*" Concentration")
-#   } else if (parameter == "pressure") {
-#     ylab <- "Barometric Pressure (hPa)"
-#     title <- "Atmospheric Pressure"
-#   } else {
-#     ylab <- parameter
-#     title <- parameter
-#   }
-# 
-#   # ----- Data Preparation ----------------------------------------------------
-# 
-#   # Default to use GMT if >1 timezone in data
-#   if (length(unique(df$timezone))>1) {
-#     print("More than one time zone, so forced to plot using GMT")
-#     useGMT <- TRUE
-#   }
-# 
-#   # Set time axis data and labels
-#   if (useGMT) {
-#     datetime <- df$datetime
-#     xlab <- xlabGMT
-#   } else {
-#     datetime <- lubridate::with_tz(df$datetime, tzone=df$timezone[1])
-#     xlab <- xlabLocal
-#   }
-# 
-#   # Time limit application
-#   if (!is.null(tlim)) {
-#     # TODO: add logic to check for tlim format
-#     # TODO: warn if tlim is outside range of datetime data
-#     timeMask <- datetime >= lubridate::ymd(tlim[1]) & datetime < lubridate::ymd(tlim[2])+lubridate::days(1)
-#     if (sum(timeMask)==0) {
-#       stop("No data contained within specified time limits, please try again.")
-#     }
-#     datetime <- datetime[timeMask]
-#     df <- df[timeMask,]
-#   }
-# 
-#   # Prep the data to plot, based on parameter selection by user (default = "pm25")
-#   param <- df[[parameter]]
-# 
-#   # ----- Plotting ------------------------------------------------------------
-# 
-#   # Create the plot
-#   
-#   if (shadedNight==TRUE || !is.null(shadedBackground)) {
-#     plot(datetime,param,
-#          type="n",
-#          xlab=xlab,
-#          ylab=ylab)
-#   }
-#   
-#   # Shaded Night: based on first deployment lat/lon if >1 deployment; breaks if >1 time zone
-#   if (shadedNight) {
-#     if (length(unique(df$timezone))>1) {
-#       stop("Can't do shaded night for more than one time zone!!")
-#     } else {
-#       lon <- df$longitude[1]
-#       lat <- df$latitude[1]
-#       timezone <- df$timezone[1]
-#       if (useGMT) {
-#         timeInfo <- PWFSLSmoke::timeInfo(df$datetime, lon, lat, timezone)
-#         PWFSLSmoke::addShadedNights(timeInfo)
-#       } else {
-#         timeInfo <- PWFSLSmoke::timeInfo(datetime, lon, lat, timezone)
-#         PWFSLSmoke::addShadedNights(timeInfo)
-#       }
-#     }
-#   }
-# 
-#   # Shaded Background
-#   # TODO: add shadedBackground in the same manner as above
-#   if (!is.null(shadedBackground)) {
-#     addShadedBackground(param=df[[shadedBackground]], timeAxis=datetime, lwd=sblwd, ...)
-#   }
-# 
-#   # Create the actual data plot (on top of background shading if it exists)
-#   lines(datetime,param,
-#        #xlab=xlab,
-#        #ylab=ylab,
-#        type=type,
-#        lwd=linelwd,
-#        ...)
-#   
-#   # Add chart title
-#   title(title)
-# 
-# }
+
+rawPlot_timeseries <- function(df,
+                               parameter="pm25",
+                               useGMT=FALSE,
+                               shadedNight=FALSE,
+                               shadedBackground=NULL, #specify parameter to shade
+                               #add=FALSE,
+                               tlim=NULL,
+                               type=NULL,
+                               linelwd=4,
+                               sblwd=1,
+                               ...) {
+
+  # If passed as argument, check that parameter exists in names of df
+  if (!(parameter %in% names(df))) {
+    stop(paste0("'",parameter,"' does not exist in names(",deparse(substitute(df)),")",sep=""))
+  }
+
+  # ----- Style ---------------------------------------------------------------
+
+  # Time axis labels
+  xlabLocal <- "Date and Time (local)" # used if GMT==FALSE
+  xlabGMT <- "Date and Time (GMT)" # used if GMT==TRUE, or if time data is in >1 timezone
+
+  # Line type default (can be overwritten)
+  if(is.null(type)) {type <- "l"; typeSpec <- FALSE} else {typeSpec <- TRUE}
+
+  # Parameter-specific styling
+  if (parameter == "temperature") {
+    ylab <- "Air Temperature (Deg C)"
+    title <- "Air Temperature"
+  } else if (parameter == "humidity") {
+    ylab <- "Relative Humidity (%)"
+    title <- "Relative Humidity"
+  } else if (parameter == "windSpeed") {
+    ylab <- "Wind Speed (m/s)"
+    title <- "Wind Speed"
+  } else if (parameter == "windDir") {
+    ylab <- "Wind Direction (degrees)"
+    if (!typeSpec) {type <- "p"} # change from line graph to dots unless specifically requested to plot as line
+    title <- "Wind Direction"
+  } else if (parameter == "pm25") {
+    ylab <- expression(paste("PM"[2.5]*" (",mu,"g/m"^3*")"))
+    title <- expression("PM"[2.5]*" Concentration")
+  } else if (parameter == "pressure") {
+    ylab <- "Barometric Pressure (hPa)"
+    title <- "Atmospheric Pressure"
+  } else {
+    ylab <- parameter
+    title <- parameter
+  }
+
+  # ----- Data Preparation ----------------------------------------------------
+
+  # Default to use GMT if >1 timezone in data
+  if (length(unique(df$timezone))>1) {
+    print("More than one time zone, so forced to plot using GMT")
+    useGMT <- TRUE
+  }
+
+  # Set time axis data and labels
+  if (useGMT) {
+    datetime <- df$datetime
+    xlab <- xlabGMT
+  } else {
+    datetime <- lubridate::with_tz(df$datetime, tzone=df$timezone[1])
+    xlab <- xlabLocal
+  }
+
+  # Time limit application
+  if (!is.null(tlim)) {
+    # TODO: add logic to check for tlim format
+    # TODO: warn if tlim is outside range of datetime data
+    timeMask <- datetime >= lubridate::ymd(tlim[1]) & datetime < lubridate::ymd(tlim[2])+lubridate::days(1)
+    if (sum(timeMask)==0) {
+      stop("No data contained within specified time limits, please try again.")
+    }
+    datetime <- datetime[timeMask]
+    df <- df[timeMask,]
+  }
+
+  # Prep the data to plot, based on parameter selection by user (default = "pm25")
+  param <- df[[parameter]]
+
+  # ----- Plotting ------------------------------------------------------------
+
+  # Create the plot
+
+  if (shadedNight==TRUE || !is.null(shadedBackground)) {
+    plot(datetime,param,
+         type="n",
+         xlab=xlab,
+         ylab=ylab)
+  }
+
+  # Shaded Night: based on first deployment lat/lon if >1 deployment; breaks if >1 time zone
+  if (shadedNight) {
+    if (length(unique(df$timezone))>1) {
+      stop("Can't do shaded night for more than one time zone!!")
+    } else {
+      lon <- df$longitude[1]
+      lat <- df$latitude[1]
+      timezone <- df$timezone[1]
+      if (useGMT) {
+        timeInfo <- PWFSLSmoke::timeInfo(df$datetime, lon, lat, timezone)
+        PWFSLSmoke::addShadedNights(timeInfo)
+      } else {
+        timeInfo <- PWFSLSmoke::timeInfo(datetime, lon, lat, timezone)
+        PWFSLSmoke::addShadedNights(timeInfo)
+      }
+    }
+  }
+
+  # Shaded Background
+  # TODO: add shadedBackground in the same manner as above
+  if (!is.null(shadedBackground)) {
+    addShadedBackground(param=df[[shadedBackground]], timeAxis=datetime, lwd=sblwd, ...)
+  }
+
+  # Create the actual data plot (on top of background shading if it exists)
+  lines(datetime,param,
+       #xlab=xlab,
+       #ylab=ylab,
+       type=type,
+       lwd=linelwd,
+       ...)
+
+  # Add chart title
+  title(title)
+
+}
--- R/raw_enhance.R
+++ R/raw_enhance.R
@@ -1,84 +1,85 @@
-# #' @keywords raw_enhance
-# #' @export
-# #' @title Process Raw Monitoring Data to Create raw_enhance Object
-# #' @param raw raw monitor data, as created by airsis_createRawDataframe or wrcc_createRawDataframe
-# #' @param rawSource source of raw monitor data (i.e. "AIRSIS" or "WRCC")
-# #' @description Processes raw monitor data to create a consistent format that can be handled by various raw* functions.
-# #' All original raw data is retained, and additional columns are added with names add consistently-named columns enabling
-# #' processing from various sources.
-# #' @return Dataframe with original raw data, plus new columns with raw naming scheme for downstream use.
-# #' \dontrun{
-# #' raw <- airsis_createRawDataframe(startdate = 20160901,enddate=20161015, unitID = 1012)
-# #' raw <- raw_enhance(raw,rawSource='AIRSIS')
-# #' View(raw)
-# #' }
-# 
-# # TODO: Add check to ensure that column names match expected for the type passed in (e.g. EBAM_AIRSIS).
-# # TODO: update createRawDataframe to include rawSource, so don't have to call as argument here
-# # TODO: NEED TO CHECK UNITS OF MEASURE!!!! Check for all fields below.
-# 
-# raw_enhance <- function(raw, rawSource="AIRSIS") {
-# 
-#   # Identify monitor type and source and save to variable
-#   monType <- paste0(raw$monitorType[1],"_",rawSource,sep="")
-# 
-#   # Create uniform attributes
-#   if ( monType=="EBAM_AIRSIS" ) {
-#     raw$temperature <- raw$AT
-#     raw$humidity <- raw$RHx
-#     raw$windSpeed <- raw$W.S
-#     raw$windDir <- raw$W.D
-#     raw$pm25 <- raw$ConcHr*1000 # TODO: Review this field for appropriateness (e.g. could use COncRT?)
-#     raw$longitude <- raw$medoidLon
-#     raw$latitude <- raw$medoidLat
-#     raw$pressure <- as.numeric(NA)
-#     raw$dataSource <- "AIRSIS"
-#   } else if ( monType=="ESAM_AIRSIS" ) {
-#     raw$temperature <- raw$AT.C.
-#     raw$humidity <- raw$RHx...
-#     raw$windSpeed <- raw$WS.M.S.
-#     raw$windDir <- raw$WD.Deg.
-#     raw$pm25 <- raw$Conc.mg.m3.*1000
-#     raw$longitude <- raw$medoidLon
-#     raw$latitude <- raw$medoidLat
-#     raw$pressure <- (raw$BP.PA.)/100 #Adjust to hPa for consistency w/ ESAM_WRCC
-#     raw$dataSource <- "AIRSIS"
-#   } else if ( monType=="ESAM_WRCC" ) {
-#     raw$temperature <- raw$AvAirTemp
-#     raw$humidity <- raw$RelHumidity
-#     raw$windSpeed <- raw$WindSpeed
-#     raw$windDir <- raw$WindDir
-#     raw$pm25 <- raw$ConcRT*1000
-#     raw$longitude <- raw$medoidLon
-#     raw$latitude <- raw$medoidLat
-#     raw$pressure <- raw$BaromPress
-#     raw$dataSource <- "WRCC"
-#   } else {
-#     stop(paste0("Monitor type (",monType,") not currently supported.",sep=""))
-#   }
-# 
-#   # Fill missing hours
-#   df <- as.data.frame(seq(raw$datetime[1],raw$datetime[nrow(raw)],by=3600))
-#   names(df) <- "datetime"
-#   df <- dplyr::left_join(df,raw,by="datetime")
-# 
-#   # Apply timezone to hours with location information
-#   df$timezone <- ""
-#   for (i in unique(df$deploymentID)) {
-#     if (is.na(i)) { break } else {
-#       index <- which(df$deploymentID==i)
-#       df$timezone[index] <- MazamaSpatialUtils::getTimezone(df$longitude[index[1]], df$latitude[index[1]],
-#                                                             countryCodes=NULL, useBuffering=TRUE) #modified from addMazamaMetadata.R
-#     }
-#   }
-# 
-#   # Apply timezone to missing hours based on TZ of prior good hour
-#   tz <- df$timezone[1]
-#   for (i in 2:nrow(df)) {
-#     if(df$timezone[i]=="") {df$timezone[i] <- tz}
-#     tz <- df$timezone[i]
-#   }
-# 
-#   return(df)
-# 
-# }
+#' @keywords raw_enhance
+#' @export
+#' @title Process Raw Monitoring Data to Create raw_enhance Object
+#' @param raw raw monitor data, as created by airsis_createRawDataframe or wrcc_createRawDataframe
+#' @param rawSource source of raw monitor data (i.e. "AIRSIS" or "WRCC")
+#' @description Processes raw monitor data to create a consistent format that can be handled by various raw* functions.
+#' All original raw data is retained, and additional columns are added with names add consistently-named columns enabling
+#' processing from various sources.
+#' @return Dataframe with original raw data, plus new columns with raw naming scheme for downstream use.
+#' @examples
+#' \dontrun{
+#' raw <- airsis_createRawDataframe(startdate = 20160901,enddate=20161015, unitID = 1012)
+#' raw <- raw_enhance(raw,rawSource='AIRSIS')
+#' View(raw)
+#' }
+
+# TODO: Add check to ensure that column names match expected for the type passed in (e.g. EBAM_AIRSIS).
+# TODO: update createRawDataframe to include rawSource, so don't have to call as argument here
+# TODO: NEED TO CHECK UNITS OF MEASURE!!!! Check for all fields below.
+
+raw_enhance <- function(raw, rawSource="AIRSIS") {
+
+  # Identify monitor type and source and save to variable
+  monType <- paste0(raw$monitorType[1],"_",rawSource,sep="")
+
+  # Create uniform attributes
+  if ( monType=="EBAM_AIRSIS" ) {
+    raw$temperature <- raw$AT
+    raw$humidity <- raw$RHx
+    raw$windSpeed <- raw$W.S
+    raw$windDir <- raw$W.D
+    raw$pm25 <- raw$ConcHr*1000 # TODO: Review this field for appropriateness (e.g. could use COncRT?)
+    raw$longitude <- raw$medoidLon
+    raw$latitude <- raw$medoidLat
+    raw$pressure <- as.numeric(NA)
+    raw$dataSource <- "AIRSIS"
+  } else if ( monType=="ESAM_AIRSIS" ) {
+    raw$temperature <- raw$AT.C.
+    raw$humidity <- raw$RHx...
+    raw$windSpeed <- raw$WS.M.S.
+    raw$windDir <- raw$WD.Deg.
+    raw$pm25 <- raw$Conc.mg.m3.*1000
+    raw$longitude <- raw$medoidLon
+    raw$latitude <- raw$medoidLat
+    raw$pressure <- (raw$BP.PA.)/100 #Adjust to hPa for consistency w/ ESAM_WRCC
+    raw$dataSource <- "AIRSIS"
+  } else if ( monType=="ESAM_WRCC" ) {
+    raw$temperature <- raw$AvAirTemp
+    raw$humidity <- raw$RelHumidity
+    raw$windSpeed <- raw$WindSpeed
+    raw$windDir <- raw$WindDir
+    raw$pm25 <- raw$ConcRT*1000
+    raw$longitude <- raw$medoidLon
+    raw$latitude <- raw$medoidLat
+    raw$pressure <- raw$BaromPress
+    raw$dataSource <- "WRCC"
+  } else {
+    stop(paste0("Monitor type (",monType,") not currently supported.",sep=""))
+  }
+
+  # Fill missing hours
+  df <- as.data.frame(seq(raw$datetime[1],raw$datetime[nrow(raw)],by=3600))
+  names(df) <- "datetime"
+  df <- dplyr::left_join(df,raw,by="datetime")
+
+  # Apply timezone to hours with location information
+  df$timezone <- ""
+  for (i in unique(df$deploymentID)) {
+    if (is.na(i)) { break } else {
+      index <- which(df$deploymentID==i)
+      df$timezone[index] <- MazamaSpatialUtils::getTimezone(df$longitude[index[1]], df$latitude[index[1]],
+                                                            countryCodes=NULL, useBuffering=TRUE) #modified from addMazamaMetadata.R
+    }
+  }
+
+  # Apply timezone to missing hours based on TZ of prior good hour
+  tz <- df$timezone[1]
+  for (i in 2:nrow(df)) {
+    if(df$timezone[i]=="") {df$timezone[i] <- tz}
+    tz <- df$timezone[i]
+  }
+
+  return(df)
+
+}
