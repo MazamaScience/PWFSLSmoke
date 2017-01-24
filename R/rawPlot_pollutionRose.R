63a8409327adfb1d33e26390823d694449045686
Rex Thompson <rexs.thompson@gmail.com>
2017-01-24 11:56:25 -0800

    rawPlot_pollutionRose and rawPlot_windRose overhauls

--- R/rawPlot_pollutionRose.R
+++ R/rawPlot_pollutionRose.R
@@ -1,54 +1,92 @@
-# # NOTE: this is a placeholder for now.  Basic concept is captured, and will develop further at a later date.
-# 
-# # IDEAS
-# # Subset data by time?
-# # Move around title, labels, etc.
-# # change colors?
-# 
-# # NOTE: Can use this function to plot various parameters against wind direction
-# # NOTE: As an interesting check on the binning by wind direction, run the function below with parameter="windDir".  It
-# # NOTE: is interesting to note that the boundary bins do not fully capture; so the direction bounds must be offset!
-# 
-# # use the following to set up some dummy raw_enhanced data to play with
-# if (FALSE) {
-#   
-#   library(PWFSLSmoke)
-#   library(openair)
-#   setwd("~/Projects/PWFSLSmoke/")
-#   
-#   load("localData/airsis_rawList.RData")
-#   source('~/Projects/PWFSLSmoke/R/raw_enhanced.R', echo=FALSE)
-#   
-#   raw <-   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
-#   ebam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   raw <- airsis_rawList$Naches; rawSource <- "AIRSIS" #ESAM AIRSIS
-#   esam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   raw <- airsis_rawList$Usk; rawSource <- "WRCC" #ESAM WRCC
-#   esam_wrcc <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   rm(raw)
-#   rm(rawSource)
-#   
-# }
-# 
-# rawPlot_pollutionRose <- function(raw, parameter="pm25", normalise=TRUE, ...) {
-#   
-#   openair::pollutionRose(raw, ws="windSpeed", wd="windDir",pollutant=parameter, normalise=normalise, angle=15, ...)
-#   # NOTE: unclear why we need to pass in a wind speed argument since not used in pollution rose...?!?
-# 
-# }
-# 
-# #as an alternative, the following may be useful...perhaps as small multiples by day?
-# #polarPlot(ebam_airsis,pollutant="pm25",x="windSpeed",wd="windDir")
-# 
-# #here's another type of openair plot that may prove handy, or at least inspire a similar plot
-# summaryPlot(ebam_airsis[c("datetime","pm25","windSpeed","windDir")],period = "months")
-# 
-# percentileRose(esam_wrcc,pollutant = "pm25",wd="windDir")
-# 
-# #polarAnnulus(ebam_airsis,pollutant="pm25") #can't get this one to work, but looks cool
-# 
-# names(ebam_airsis)
-# ebam_airsis$ws <- NULL
+#' @keywords raw_enhance
+#' @export
+#' @title Create a Pollution Rose Plot from raw_enhance Object
+#' @param df raw_enhance monitor object, as created by the raw_enhance() function
+#' @param parameter parameter to plot
+#' @param tlim A vector that subsets the raw dataframe by time limits, e.g. c(20160915,20161010)
+#' @param localTime use local times; defaults to GMT if >1 time zone in data.
+#' @param normalize normalize slices to fill entire area, allowing for easier comparison of counts of magnitudes by direction
+#' @param ... additional arguments to pass on to openair::pollutionRose()
+#' @description Create pollution rose plot from raw_enhance object based on openair::pollutionRose(). If normalized, black line
+#' indicates frequency by direction.
+#' @examples
+#' \dontrun{
+#' df <- PWFSLSmoke::airsis_createRawDataframe(startdate = 20160901, enddate=20161015, unitID = 1012)
+#' df <- raw_enhance(df,rawSource='AIRSIS')
+#' rawPlot_pollutionRose(df)
+#' }
+
+rawPlot_pollutionRose <- function(df,
+                                  parameter="pm25",
+                                  tlim=NULL,
+                                  localTime=TRUE,
+                                  normalize=FALSE,
+                                  ...) {
+  
+  # ----- Data Preparation -----------------
+  
+  # Identify timezone(s)
+  timezone <- unique(df$timezone)
+  
+  # Force timezone to UTC if >1 timezone in metadata for monitorIDs
+  if ( length(timezone)>1 ) { # note that we will only enter this condition if localTime==TRUE
+    if ( localTime ) {
+      warning(">1 timezone in data: Timezone (including tlim, if specified) forced to UTC")
+      timezone <- "UTC"
+    }
+  }
+  
+  # Set timezone to UTC if localTime==FALSE
+  if ( !localTime ) {
+    timezone <- "UTC"
+  }
+  
+  # Set time axis data
+  df$datetime <- lubridate::with_tz(df$datetime, tzone=timezone)
+  
+  # Time limit application
+  # TODO: add logic to check for tlim format
+  # TODO: warn if tlim is outside range of datetime data
+  if ( !is.null(tlim) ) {
+    # When tlim is specified in whole days we add hours to get the requsted full days
+    tlimStrings <- as.character(tlim)
+    if ( stringr::str_length(tlimStrings)[1] == 8 ) {
+      tlim[1] <- paste0(tlim[1],'00')
+    }
+    if ( stringr::str_length(tlimStrings)[2] == 8 ) {
+      tlim[2] <- paste0(tlim[2],'23')
+    }
+    tlim <- PWFSLSmoke::parseDatetime(tlim, timezone=timezone)
+    
+    # Create time mask and subset data
+    timeMask <- df$datetime >= tlim[1] & df$datetime <= tlim[2]
+    if ( sum(timeMask)==0 ) {
+      stop("No data contained within specified time limits, please try again.")
+    }
+    df <- df[timeMask,]
+  }
+  
+  # Populate arguments for openair::windRose
+  argsList <- list(...)
+  argsList$mydata <- df
+  argsList$pollutant <- parameter
+  argsList$normalise <- normalize
+  if ( !('angle' %in% names(argsList)) ) {
+    argsList$angle <- 15
+  }
+  if ( !('ws' %in% names(argsList)) ) {
+    argsList$ws <- "windSpeed"
+  }
+  if ( !('wd' %in% names(argsList)) ) {
+    argsList$wd <- "windDir"
+  }
+  
+  # ----- Plotting -------------------------
+  
+  # Create Plot
+  do.call(openair::pollutionRose,argsList)
+  
+}
+
+# NOTE: As an interesting check on the binning by wind direction, run the function above with parameter="windDir". It
+# NOTE: is interesting to note that the boundary bins do not fully capture; so the direction bounds must be offset!
--- R/rawPlot_windRose.R
+++ R/rawPlot_windRose.R
@@ -1,37 +1,82 @@
-# # NOTE: this is a placeholder for now.  Basic concept is captured, and will develop further at a later date.
-# 
-# # IDEAS
-# # Subset by time?
-# # Move around title, labels, etc.
-# # change colors?
-# 
-# # use the following to set up some dummy raw_enhanced data to play with
-# if (FALSE) {
-#   
-#   library(PWFSLSmoke)
-#   library(openair)
-#   setwd("~/Projects/PWFSLSmoke/")
-#   
-#   load("localData/airsis_rawList.RData")
-#   source('~/Projects/PWFSLSmoke/R/raw_enhanced.R', echo=FALSE)
-#   
-#   raw <-   raw <- airsis_rawList$Plain; rawSource <- "AIRSIS" #EBAM AIRSIS
-#   ebam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   raw <- airsis_rawList$Naches; rawSource <- "AIRSIS" #ESAM AIRSIS
-#   esam_airsis <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   raw <- airsis_rawList$Usk; rawSource <- "WRCC" #ESAM WRCC
-#   esam_wrcc <- raw_enhanced(raw, rawSource = rawSource)
-#   
-#   rm(raw)
-#   rm(rawSource)
-#   
-# }
-# 
-# rawPlot_windRose <- function(raw, ...) {
-#   
-#   openair::windRose(raw,ws="windSpeed",wd="windDir",angle=15, ...)
-# 
-# }
-# 
+#' @keywords raw_enhance
+#' @export
+#' @title Create a Wind Rose Plot from raw_enhance Object
+#' @param df raw_enhance monitor object, as created by the raw_enhance() function
+#' @param tlim A vector that subsets the raw dataframe by time limits, e.g. c(20160915,20161010)
+#' @param localTime use local times; defaults to GMT if >1 time zone in data.
+#' @param ... additional arguments to pass on to openair::windRose()
+#' @description Create wind rose plot from raw_enhance object. Based on openair::windRose().
+#' @examples
+#' \dontrun{
+#' df <- PWFSLSmoke::airsis_createRawDataframe(startdate = 20160901,enddate=20161015, unitID = 1012)
+#' df <- raw_enhance(df,rawSource='AIRSIS')
+#' rawPlot_windRose(df)
+#' }
+
+rawPlot_windRose <- function(df,
+                             tlim=NULL,
+                             localTime=TRUE,
+                             ...) {
+  
+  # ----- Data Preparation -----------------
+  
+  # Identify timezone(s)
+  timezone <- unique(df$timezone)
+  
+  # Force timezone to UTC if >1 timezone in metadata for monitorIDs
+  if ( length(timezone)>1 ) { # note that we will only enter this condition if localTime==TRUE
+    if ( localTime ) {
+      warning(">1 timezone in data: Timezone (including tlim, if specified) forced to UTC")
+      timezone <- "UTC"
+    }
+  }
+  
+  # Set timezone to UTC if localTime==FALSE
+  if ( !localTime ) {
+    timezone <- "UTC"
+  }
+  
+  # Set time axis data
+  df$datetime <- lubridate::with_tz(df$datetime, tzone=timezone)
+  
+  # Time limit application
+  # TODO: add logic to check for tlim format
+  # TODO: warn if tlim is outside range of datetime data
+  if ( !is.null(tlim) ) {
+    # When tlim is specified in whole days we add hours to get the requsted full days
+    tlimStrings <- as.character(tlim)
+    if ( stringr::str_length(tlimStrings)[1] == 8 ) {
+      tlim[1] <- paste0(tlim[1],'00')
+    }
+    if ( stringr::str_length(tlimStrings)[2] == 8 ) {
+      tlim[2] <- paste0(tlim[2],'23')
+    }
+    tlim <- PWFSLSmoke::parseDatetime(tlim, timezone=timezone)
+    
+    # Create time mask and subset data
+    timeMask <- df$datetime >= tlim[1] & df$datetime <= tlim[2]
+    if ( sum(timeMask)==0 ) {
+      stop("No data contained within specified time limits, please try again.")
+    }
+    df <- df[timeMask,]
+  }
+  
+  # Populate arguments for openair::windRose
+  argsList <- list(...)
+  argsList$mydata <- df
+  if ( !('angle' %in% names(argsList)) ) {
+    argsList$angle <- 15
+  }
+  if ( !('ws' %in% names(argsList)) ) {
+    argsList$ws <- "windSpeed"
+  }
+  if ( !('wd' %in% names(argsList)) ) {
+    argsList$wd <- "windDir"
+  }
+  
+  # ----- Plotting -------------------------
+  
+  # Create Plot
+  do.call(openair::windRose,argsList)
+  
+}
