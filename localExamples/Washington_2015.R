759f2ab7a014184077bcc27fe3d42e36b10263af
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2017-02-14 13:47:01 -0800

    commit before merge

---
+++ localExamples/Washington_2015.R
@@ -0,0 +1,68 @@
+# Washington_2015
+#
+# Various functions for a Washgington Summer of 2015 project
+#
+
+library(PWFSLSmoke)
+
+logger.setup()
+logger.setLevel(INFO)
+
+library(MazamaSpatialUtils)
+setSpatialDataDir('~/Data/Spatial')
+loadSpatialData('NaturalEarthAdm1')
+
+wrcc_getData <- function(startdate=20150601, enddate=20150930) {
+  
+  # Get all of the WRCC cache monitors
+  # NOTE:  No cache monitors data is available for summer of 2015
+  cacheList <- list()
+  i <- 0
+  for ( stationID in WRCC$cache ) {
+    logger.info('Working on %s', stationID)
+    i <- i + 1
+    result <- try( mon <- wrcc_createMonitorObject(startdate, enddate, stationID),
+                   silent=TRUE )
+    if ( "try-error" %in% class(result) ) {
+      next
+    } else {
+      cacheList[[i]] <- mon
+    }
+  }
+  cache <- monitor_combine(cacheList)
+  
+  # Get all of the WRCC miscellaneous monitors
+  # NOTE:  No miscellaneous monitors data is available for summer of 2015
+  miscellaneousList <- list()
+  i <- 0
+  for ( stationID in WRCC$cache ) {
+    logger.info('Working on %s', stationID)
+    i <- i + 1
+    result <- try( mon <- wrcc_createMonitorObject(startdate, enddate, stationID),
+                   silent=TRUE )
+    if ( "try-error" %in% class(result) ) {
+      next
+    } else {
+      miscellaneousList[[i]] <- mon
+    }
+  }
+  miscellaneous <- monitor_combine(miscellaneousList)
+  
+  # Get all of the WRCC USFS regional monitors
+  # NOTE:  No usfs_regional monitors data is available for summer of 2015
+  usfs_regionalList <- list()
+  i <- 0
+  for ( stationID in WRCC$usfs_regional ) {
+    logger.info('Working on %s', stationID)
+    i <- i + 1
+    result <- try( mon <- wrcc_createMonitorObject(startdate, enddate, stationID),
+                   silent=TRUE )
+    if ( "try-error" %in% class(result) ) {
+      next
+    } else {
+      usfsRegionalList[[i]] <- mon
+    }
+  }
+  usfs_regional <- monitor_combine(usfs_regionalList)
+  
+}
