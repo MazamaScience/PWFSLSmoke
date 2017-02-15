e41426294fc18c52217255767c30c25bcb327c48
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2017-02-15 13:48:21 -0800

    added airnow_loadLatest ...
     * monitor_collapse now has na.rm=TRUE argument

--- R/monitor_collapse.R
+++ R/monitor_collapse.R
@@ -1,23 +1,26 @@
-#' @keywords internal
+#' @keywords ws_monitor
 #' @export
 #' @title Collapse a ws_monitor Object into a ws_monitor Object with a Single Monitor
 #' @param ws_monitor ws_monitor object
 #' @param lon longitude of the collapsed monitoring station. (Default = mean of the longitudes)
 #' @param lat latitude of the collapsed monitoring station. (Default = mean of the latitudes)
-#' @param monitorID monitor ID of the collapsed monitoring station. (Default = 'generated_id')
-#' @param FUN function to be applied to all the monitors at a single time index. (Default = mean)
+#' @param monitorID monitor ID of the collapsed monitoring station.
+#' @param FUN function to be applied to all the monitors at a single time index.
+#' @param na.rm logical value indicating whether NA values should be ignored when FUN is applied
 #' @param ... additional arguments to be passed on to the apply() function
 #' @return A ws_monitor object with meta and data that corresponds to the collapsed single monitor
-#' @description Collapses all the monitors from a given ws_monitor object into a single-monitor ws_monitor object using
-#' the function provided in the \code{FUN} argument. The collapse applies to both ws_monitor$meta and ws_monitor$data.
-#'
-#' Intended for use with gridded model data, rather than actual monitor data itself (meta data is largely chopped; NAs propagate).
+#' @description Collapses data from all the monitors in \code{ws_monitor} into a single-monitor 
+#' ws_monitor object using the function provided in the \code{FUN} argument. The single-monitor
+#' result will be located at the mean longitude and latitude unless \code{lon} and \code{lat}
+#' parameters are specified.
 
-monitor_collapse <- function(ws_monitor, lon=NULL, lat=NULL, monitorID='generated_id', FUN=mean, ...) {
+monitor_collapse <- function(ws_monitor, lon=NULL, lat=NULL,
+                             monitorID='generated_id',
+                             FUN=mean, na.rm=TRUE, ...) {
   
   data <- ws_monitor$data[,-1]
   
-  collapsed_data <- apply(data, MARGIN = 1, FUN = FUN, ...)
+  collapsed_data <- apply(data, MARGIN=1, FUN=FUN, na.rm=na.rm, ...)
  
   newData <- data.frame(ws_monitor$data$datetime, collapsed_data)
   colnames(newData) <- c('datetime', monitorID)
