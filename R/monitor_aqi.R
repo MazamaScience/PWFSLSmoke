e084d8e7a016b90dcba98e9a38f65e3daee50788
Rex Thompson <RexS.Thompson@gmail.com>
2017-08-16 15:58:30 -0700

    aqi work

--- R/monitor_aqi.R
+++ R/monitor_aqi.R
@@ -19,10 +19,47 @@ monitor_aqi <- function(ws_monitor, aqiParameter='pm25', nowcastVersion='pm', in
   
   # TODO: implement AQI calculation
   
+  
+  pmTable <- data.frame(rangeLow=c(0.0, 12.1, 35.5, 55.5, 150.5, 250.5, 350.5),
+                        rangeHigh=c(12.0, 35.4, 55.4, 150.4, 250.4, 350.4, 500.4),
+                        aqiLow=c(0, 51, 101, 151, 201, 301, 401),
+                        aqiHigh=c(50, 100, 150, 200, 300, 400, 500))
+  
+
+  data <- c(74, 155, 259)
+  data <- ws_monitor$data[,2]
+  C_p <- 75
+  
+  getAQI <- function(C_p) {
+    
+    rowIndex <- which(pmTable$rangeLow < C_p & C_p < pmTable$rangeHigh)
+    
+    if ( length(rowIndex)==0 ) {
+      print("wah wah")
+      AQI_val <- NA
+    } else {
+      BP_Hi <- pmTable$rangeHigh[rowIndex]
+      BP_Lo <- pmTable$rangeLow[rowIndex]
+      I_Hi <- pmTable$aqiHigh[rowIndex]
+      I_Lo <- pmTable$aqiLow[rowIndex]
+      
+      I_p <- (I_Hi-I_Lo)/(BP_Hi-BP_Lo)*(C_p-BP_Lo)+I_Lo
+      
+      
+    }
+  }
+  
 
   
-  
-  
+
   return(ws_monitor)
   
 }
+
+# # 40 CFR 58 Equation 1
+# .eqn1_40CFR58_calcAQI <- function(C_p, BP_Hi, BP_Lo, I_Hi, I_Lo) {
+#   I_p <- (I_Hi-I_Lo)/(BP_Hi-BP_Lo)*(C_p-BP_Lo)+I_Lo
+#   return(I_p)
+# }
+
+# .eqn1_40CFR58_calcAQI(C_p, BP_Hi, BP_Lo, I_Hi, I_Lo)
