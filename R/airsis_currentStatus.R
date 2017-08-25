19d200b6a33ccbd96b65fc5aedd740e187d46ee9
Rex Thompson <RexS.Thompson@gmail.com>
2017-08-25 10:49:58 -0700

    currentStatus updates for time adjustments and added documentation

--- R/airsis_currentStatus.R
+++ R/airsis_currentStatus.R
@@ -1,39 +1,31 @@
+#' @export
+#' @title Load Current Status from Airsis Site
+#' @description Scrape current monitor status from Airsis website (http://xxxx.airsis.com
+#' where xxxx %in% c("usfs", "apcd", "arb2", "epa").
+#' @param provider data provider, e.g. usfs, arb2, etc.
+#' @param username username for login credentials
+#' @param password password for login credentials
+#' @return A dataframe with unit ID, alias, location, and latest update time, in UTC.
+#' @examples
+#' \dontrun{
+#' usfs <- airsis_currentStatus(provider="usfs")
+#' View(usfs)
+#' }
 
-# #' @param provider data provider, e.g. usfs, arb2, etc.
-# #' 
-
-# This function will use the rvest package to parse the table found at URLs like  http://usfs.airsis.com.
-# It should accept the following arguments:
-#   provider (usfs, arb2, etc.)
-# It should return a data frame with the following columns:
-#   unitID
-#   alias
-#   location
-#   datetimeString
-
-arisis_currentStatus <- function(provider, username, password) {
+airsis_currentStatus <- function(provider, username, password) {
   
   # TODO: develop code to get response via request, cookies, etc...
   ### response <- httr::GET("http://usfs.airsis.com")  #, authenticate(username, password))
   ### response <- httr::GET("http://usfs.airsis.com/vision/Login.aspx?ReturnUrl=%2fvision", authenticate(username, password))
   
   ### ---- temporary for development/testing purposes ------------------
-  providers <- c("USFS", "APCD", "ARB2", "EPA")
-  provider <- providers[4]
+  # providers <- c("usfs", "apcd", "arb2", "epa")
+  # provider <- providers[4]
   response <- readr::read_file(paste0("~/Downloads/", provider, ".htm"))
   ### ------------------------------------------------------------------
   
-  # provider-specific settings
-  if ( provider=="USFS") {
-    linkNode <- "#DataGrid1 td:nth-child(4) a"
-  } else if ( provider=="APCD") {
-    linkNode <- "td:nth-child(21) a"
-  } else if ( provider=="ARB2") {
-    linkNode <- "td:nth-child(21) a"
-  } else if ( provider=="EPA") {
-    linkNode <- "td:nth-child(20) a"
-  }
-  
+  provider <- stringr::str_to_lower(provider)
+    
   # parse raw html and pull out fields of interest
   currentStatusDoc <- xml2::read_html(response)
   currentStatusNode <- rvest::html_nodes(currentStatusDoc, xpath='//*[@id="DataGrid1"]')
@@ -42,37 +34,35 @@ arisis_currentStatus <- function(provider, username, password) {
   currentStatusTable <- rvest::html_table(currentStatusNode, header=TRUE)[[1]]
   
   # get unitIDs from node
-  urls <- currentStatusDoc %>%
-    html_nodes(linkNode) %>%
-    html_attr('href')
-  
+  if ( provider=="usfs") {
+    linkNode <- "#DataGrid1 td:nth-child(4) a"
+  } else if ( provider=="apcd") {
+    linkNode <- "td:nth-child(21) a"
+  } else if ( provider=="arb2") {
+    linkNode <- "td:nth-child(21) a"
+  } else if ( provider=="epa") {
+    linkNode <- "td:nth-child(20) a"
+  } else {
+    stop('provider not supported')
+  }
+  urls <- rvest::html_nodes(currentStatusDoc, linkNode)
+  urls <- rvest::html_attr(urls, 'href')
   unitIDs <- stringr::str_replace(urls, stringr::fixed("UnitHistory.aspx?uid="), "")
   
+  # get times
+  # TODO: verify airsis data is in local time
+  times <- currentStatusTable[["Date/Time"]]
+  times <- as.POSIXct(times, format="%m/%d/%y %I:%M%p")
+  times <- lubridate::with_tz(times, "UTC")
+  times <- format(times, "%Y-%m-%d %H:%M %Z")
+  
+  
   # create dataframe
+  df <- data.frame(unitID=unitIDs)
+  df$Alias <- currentStatusTable[["Alias"]]
+  df$Location <- currentStatusTable[["Location"]]
+  df$datetimeString <- times
   
-  dataTable <- data.frame(unitID=unitIDs)
-  dataTable$Alias <- currentStatusTable[["Alias"]]
-  dataTable$Location <- currentStatusTable[["Location"]]
-  # TODO: clean up date time, including convert to string
-  # TODO: account for UTC when possible (APCD, ARB2)
-  dataTable$datetimeString <- currentStatusTable[["Date/Time"]]
-  View(dataTable)
+  return(df)
   
-  
-
 }
-
-# tables <- rvest::html_nodes(currentStatusDoc, "table")
-# currentStatusTable <- rvest::html_table(tables[[4]], fill=TRUE)
-
-
-# if ( provider=="USFS") {
-#   xpath <- '//*[@id="DataGrid1"]'
-# } else if ( provider=="APCD") {
-#   xpath <- '//*[@id="DataGrid1"]'
-# } else if ( provider=="ARB2") {
-#   xpath <- '//*[@id="DataGrid1"]'
-# } else if ( provider=="EPA") {
-#   xpath <- '//*[@id="DataGrid1"]'
-# }
-
