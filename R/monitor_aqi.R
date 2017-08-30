a944722309171133010956e41cb667f4dfcf27f6
Rex Thompson <RexS.Thompson@gmail.com>
2017-08-30 15:59:01 -0700

    updated aqi zero adjustment to match new source, and documentation update to link to new source

--- R/monitor_aqi.R
+++ R/monitor_aqi.R
@@ -10,6 +10,7 @@
 #' @references \url{https://www3.epa.gov/airnow/aqi-technical-assistance-document-may2016.pdf}
 #' @references \url{https://archive.epa.gov/ttn/ozone/web/pdf/airqual.pdf}
 #' @references \url{https://www.ecfr.gov/cgi-bin/retrieveECFR?n=40y6.0.1.1.6#ap40.6.58_161.g}
+#' @references \url{https://forum.airnowtech.org/t/how-does-airnow-handle-negative-hourly-concentrations/143}
 #' @examples
 #' \dontrun{
 #' ws_monitor <- monitor_subset(Northwest_Megafires, tlim=c(20150815,20150831))
@@ -31,6 +32,9 @@ monitor_aqi <- function(ws_monitor, aqiParameter='pm25', nowcastVersion='pm', in
   # pull out data for AQI calculation
   n <- ncol(ws_monitor$data)
   data <- ws_monitor$data[2:n]
+  
+  # NOTE: see https://forum.airnowtech.org/t/how-does-airnow-handle-negative-hourly-concentrations/143
+  data[data<0] <- 0
   
   # TODO: include/expand checks to ensure values are appropriately truncated
   if ( aqiParameter=="pm25" || nowcastVersion=="pm" ) {
@@ -58,10 +62,6 @@ monitor_aqi <- function(ws_monitor, aqiParameter='pm25', nowcastVersion='pm', in
   # apply Equation 1 from 40 CFR 58 Appendix G and round to the nearest integer
   I_p <- (I_Hi-I_Lo)/(BP_Hi-BP_Lo)*(data-BP_Lo)+I_Lo
   I_p <- round(I_p, 0)
-  
-  # For negative concentrations, set AQI = 0
-  # NOTE: This functionality is assumed; it is not explicity referenced in 40 CFR 58
-  I_p[I_p<0] <- 0
   
   ws_monitor$data[2:n] <- I_p
   
