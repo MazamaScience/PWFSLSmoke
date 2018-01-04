5fd0f365a0bfecb6c3579956847630d7249e2289
Helen Miller <helen@ohana.org>
2018-01-03 18:30:16 -0800

    Local notebook for airsis data

--- localExecutables/airsis_getYearlyData_exec.R
+++ localExecutables/airsis_getYearlyData_exec.R
@@ -1,32 +1,101 @@
 #!/usr/bin/env Rscript
 
-library(methods)
-library(optparse)
-suppressPackageStartupMessages(library(PWFSLSmoke))
-suppressPackageStartupMessages(library(MazamaSpatialUtils))
+# Updated Jan 3, 2018
+# 
+# This script will download data for all airsis monitors for the specified time frame
+# 
+# # Example
+# airsis_getYearlyData_exec.R --startdate=20150101 --enddate=2016010223 --outputDir="~/Data/airsis" --fileName="airsis_2015"
 
+VERSION <- "0.0.1"
 
+suppressPackageStartupMessages(
+  {
+    library(methods)
+    library(optparse)
+    library(PWFSLSmoke)
+    library(MazamaSpatialUtils)
+  }
+)
+
+##########################################
+
+saveAirsisData <- function(opt) {
+  
+  # Get all unitids 
+  usfs_unitids <- airsis_availableUnits(opt$startdate, opt$enddate, provider = 'USFS')
+  apcd_unitids <- airsis_availableUnits(opt$startdate, opt$enddate, provider = 'APCD')
+  
+  # for usfs and apcd, make a list of ws_monitor objects for all units
+  monitors <- list()
+  
+  for ( provider in c("USFS", "APCD", "ARB2", "EPA")) {
+    
+    unitids <- airsis_availableUnits(opt$startdate, opt$enddate, provider = provider)
+    
+    for ( unitid in unitids ) {
+      result <- try (monitor <- airsis_createMonitorObject(startdate = opt$startdate, enddate = opt$enddate, provider = provider, unitID = unitid))
+      if ("try-error" %in% class(result)) {
+        print(paste0("error loading ", provider, " ", unitid, ": ", geterrmessage()))
+      } else {
+        monitors[[paste0(unitid, "_", provider)]] <- monitor
+      }
+    }
+    
+  }
+  
+  all_monitors <- monitor_combine(monitors)
+  
+  # Set the name of all_monitors to fileName
+  if (is.null(opt$fileName)) {
+    fileName <- paste0('airsis_', opt$startdate, "_", opt$enddate)
+  } else {
+    fileName <- opt$fileName
+  }
+  
+  assign(fileName, all_monitors)
+  filePath <- paste0(opt$outputDir, '/', fileName, '.RData')
+  
+  # Save the RData file
+  save(list = fileName, file = filePath)
+  
+}
+
+################################################################################
+# Main program
+
+# ----- Parse command line options ---------------------------------------------
+
+# Set up option parser
 option_list <- list(
-  make_option(c("--startdate"),default='20160101', help="starting date of the data to be downloaded"),
-  make_option(c("--enddate"), default='20170101', help="ending date of the data to be downloaded"),
+  make_option(c("--startdate"),default=NULL, help="starting date of the data to be downloaded"),
+  make_option(c("--enddate"), default=NULL, help="ending date of the data to be downloaded"),
   make_option(c("--outputDir"), default=getwd(), help="Output directory for generated RData files [default=\"%default\"]"),
   make_option(c("--fileName"), default=NULL, help="name for the RData file"),
   make_option(c("--logDir"), default=getwd(), help="Output directory for generated .log file [default=\"%default\"]"),
-  make_option(c("--spatialDataDir"), default='~/Data/Spatial', help="Directory containing spatial datasets used by MazamaSpatialUtils [default=\"%default\"]")
+  make_option(c("--spatialDataDir"), default='~/Data/Spatial', help="Directory containing spatial datasets used by MazamaSpatialUtils [default=\"%default\"]"),
+  make_option(c("-V","--version"), action="store_true", default=FALSE, help="Print out version number [default\"%default\"]")
 )
 
 # Parse arguments
 opt <- parse_args(OptionParser(option_list=option_list))
 
+# Print out version and quit
+if ( opt$version ) {
+  cat(paste0('createCSV_exec.R ',VERSION,'\n'))
+  quit()
+}
+
 # Sanity checks
-if ( is.null(opt$fileName) ) stop(paste0("'filePath' must be specified"))
+if ( is.null(opt$startdate) ) stop(paste0("startdate is required"))
+if ( is.null(opt$enddate) ) stop(paste0("enddate is required"))
 if ( !file.exists(opt$outputDir) ) stop(paste0("outputDir not found:  ",opt$outputDir))
 if ( !file.exists(opt$logDir) ) stop(paste0("logDir not found:  ",opt$logDir))
 
 # Assign log file names
-debugLog <- file.path(opt$logDir, paste0('openaq_createMonthlyDataframes_',opt$startdate,'_DEBUG.log'))
-infoLog  <- file.path(opt$logDir, paste0('openaq_createMonthlyDataframes_',opt$startdate,'_INFO.log'))
-errorLog <- file.path(opt$logDir, paste0('openaq_createMonthlyDataframes_',opt$startdate,'_ERROR.log'))
+debugLog <- file.path(opt$logDir, paste0('airsis_getYearlyData_', '_DEBUG.log'))
+infoLog  <- file.path(opt$logDir, paste0('airsis_getYearlyData_', '_INFO.log'))
+errorLog <- file.path(opt$logDir, paste0('airsis_getYearlyData_', '_ERROR.log'))
 
 # Set up logging
 logger.setup(debugLog=debugLog, infoLog=infoLog, errorLog=errorLog)
@@ -39,33 +108,20 @@ setSpatialDataDir(opt$spatialDataDir) ##FOR bash
 
 loadSpatialData("NaturalEarthAdm1")
 
-# Get all unitids 
-usfs_unitids <- airsis_availableUnits(opt$startdate, opt$enddate, provider = 'USFS')
-apcd_unitids <- airsis_availableUnits(opt$startdate, opt$enddate, provider = 'APCD')
 
-# for usfs and apcd, make a list of ws_monitor objects for all units
-monitors <- list()
-for ( unitid in usfs_unitids ) {
-  result <- try (monitor <- airsis_createMonitorObject(startdate = opt$startdate, enddate = opt$enddate, provider = 'USFS', unitID = unitid))
-  if ("try-error" %in% class(result)) {
-    msg <- geterrmessage()
-    print(msg)
-  } else {
-  monitors[[paste0(unitid, "_usfs")]] <- monitor
-  }
-}
-for ( unitid in apcd_unitids ) {
-  result <- try(
-    monitor <- airsis_createMonitorObject(startdate = opt$startdate, enddate = opt$enddate, provider = 'APCD', unitID = unitid)
-  )
-  if ("try-error" %in% class(result)) {
-    msg <- geterrmessage()
-    print(msg)
-  } else {
-    monitors[[paste0(unitid, "a_pcd")]] <- monitor
-  }
-}
-all_monitors <- monitor_combine(monitors)
+# ----- Save airnow ws_monitor object as a RData file ------
 
-if opt$
-save(all_monitors, file = opt$filePath)
+result <- try( saveAirsisData(opt) )
+
+if ( "try-error" %in% class(result) ) {
+  msg <- paste("Error saving airsis data: ", geterrmessage())
+  logger.fatal(msg)
+} else {
+  # Guarantee that the errorLog exists
+  if ( !file.exists(errorLog) ) dummy <- file.create(errorLog)
+  logger.info("Completed successfully!")
+}
+
+
+
+
