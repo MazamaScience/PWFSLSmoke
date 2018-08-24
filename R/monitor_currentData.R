e4098f1b55e87ad207fed639e4e108eaa4946b3a
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2018-08-23 18:18:02 -0700

    changed latency to be relative to processingTime

--- R/monitor_currentData.R
+++ R/monitor_currentData.R
@@ -7,20 +7,29 @@
 #' @description Extracts current status data from a ws_monitor object. This data includes the following:
 #' \itemize{
 #' \item{monitorID - the PWFSL monitor ID}
-#' \item{lastValidUpdateTime - the UTC timestamp corresponding to the last valid pm25 datum}
-#' \item{processingTime - the UTC timestamp that the function is run}
-#' \item{latency - the difference between processingTime and lastValidUpdateTime, floored to the hour}
-#' \item{PM2.5_nowcast - the NowCast value at lastValidUpdateTime}
-#' \item{PM2.5_latest_1 - the PM2.5 value at lastValidUpdateTime (should never be null)}
-#' \item{PM2.5_latest_3 - the mean of the three hours preceeding lastValidUpdateTime}
-#' \item{PM2.5_yesterday - the 24-hour mean from midnight-to-midnight of the day before processingTime}
+#' \item{lastValidUpdateTime - UTC POSIXct time corresponding to the last valid pm25 datum}
+#' \item{processingTime - UTC POSIXct when the function is run (= 'now')}
+#' \item{latency - (difference between processingTime -1 hr) and lastValidUpdateTime, floored to the hour}
+#' \item{PM2.5_nowcast - NowCast value at lastValidUpdateTime}
+#' \item{PM2.5_latest_1 - PM2.5 value at lastValidUpdateTime (should never be null)}
+#' \item{PM2.5_latest_3 - mean of the three hours preceeding lastValidUpdateTime}
+#' \item{PM2.5_yesterday - local time midnight-to-midnight 24-hour mean for the day prior to processingTime}
 #' }
+#' @note
+#' Data are assigned to the beginning of the hour they represent. So a PM2.5 value assigned to 2pm
+#' will represent data averaged over the period 14:00 - 14:59. This is in keeping with a day representing
+#' 00:00 - 23:59 and a month representing the beginning of the month until the end of the month.
+#' 
+#' Because of this, data for 2pm should never be available until just after 3pm. If it is currently 3:15pm
+#' then we need to subtract 1 hour from the current \code{processingTime} before subtracting the \code{lastValidUpdateTime}
+#' to generate the \code{latency}.
 #' @examples
-#' wa <- monitor_subset(Northwest_Megafires)
+#' \dontrun{
+#' wa <- loadLatest() %>% monitor_subset(stateCodes = 'WA')
 #' wa_current <- monitor_currentData(wa)
+#' }
 
 monitor_currentData <- function(ws_monitor) {
-  
   
   # Pull out data
   data <- ws_monitor$data
@@ -38,10 +47,9 @@ monitor_currentData <- function(ws_monitor) {
   nowcast <- monitor_nowcast(ws_monitor, includeShortTerm = TRUE)
   data_nowcast <- nowcast$data
   
-  
   # Add lastValidUdateTime and latency columns
 
-  lastIndex <- apply(as.matrix(data),2, function(x) { max(which(!is.na(x))) }) # this is a named vector
+  lastIndex <- apply(as.matrix(data), 2, function(x) { max(which(!is.na(x))) }) # this is a named vector
   lastUTCTime <- data$datetime[lastIndex]
   currentData$lastValidUpdateTime <- lastUTCTime[-1] # remove 'datetime'
   processingTime <- lubridate::now('UTC')
@@ -51,10 +59,9 @@ monitor_currentData <- function(ws_monitor) {
   # NOTE:  a datum assigned to 2pm represents the average of data between 2pm and 3pm.
   # NOTE:  So, if we check at 3:15 and see that we have a value for 2pm but not 3pmm 
   # NOTE:  then the data are completely up-to-date with zero latency. That's why we
-  # NOTE:  subtract an hour from 'now' in the line below.
-  nowIndex <- max(which(data$datetime <= (processingTime - lubridate::dhours(1)))) # this is a single number
-  currentData$latency <- (nowIndex - lastIndex)[-1] # remove 'datetime'
-  
+  # NOTE:  subtract an hour from 'processingTime' in the line below.
+  zeroLatencyTime <- lubridate::floor_date(processingTime, unit="hour") - lubridate::dhours(1)
+  currentData$latency <- as.numeric( difftime(zeroLatencyTime, currentData$lastValidUpdateTime, units="hour") )
   
   # ----- Add data values to currentData tbl -----------------------------------
   
