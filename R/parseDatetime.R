3a2b34a020202f86a1998c016c4014ddd7a022a4
Spencer Pease <spencer.p.816@gmail.com>
2018-10-12 13:36:50 -0700

    Update implementation of `parseDateTime`
    In addtion:
     -  add new parameter: `expectAll`
     - update documentation
    
    References issue #16

--- R/parseDatetime.R
+++ R/parseDatetime.R
@@ -1,42 +1,72 @@
 #' @export
+#'
 #' @title Parse Datetime Strings
-#' @param datetime vector of character or integer datetimes in YYYYMMDD[HHMMSS] format (or POSIXct)
-#' @param timezone Olson timezone at the location of interest
-#' @description 8-, 10-, 12- and 14-digit formats are understood, e.g: 20150721 to 20150721000000.
-#' Integers will be converted to character before parsing. All incoming dates will be interpreted
-#' in the specified \code{timezone} ("UTC" by default).
-#' 
-#' If \code{datetime} is a \code{POSIXct} it will be returned unmodified.
+#'
+#' @description
+#' Transforms numeric and string representations of YYYYMMDD[HHMMSS] datetimes
+#' to \code{POSIXct} format.
+#'
+#' Ymd, YmdH, YmdHM, and YmdHMS formats are understood, where:
+#'
+#'   * Y - four digit year
+#'   * m - decimal number (1-12, 01-12) or english name month (October, oct.)
+#'   * d - decimal number day of the month (0-31 or 01-31)
+#'   * H - decimal number hours (0-24 or 00-24)
+#'   * M - decimal number minutes (0-59 or 00-59)
+#'   * S - decimal number seconds (0-61 or 00-61)
+#'
+#' This allows for mixed inputs. For example, 20181012130900,
+#' "2018-10-12-13-09-00", and "2018 Oct. 12th 13:09:00" will all be converted to
+#' the same \code{POSIXct} datetime. The incoming datetime vector does not need
+#' to have a homogeneous format either -- "20181012" and "2018-10-12 13:09" can
+#' exist in the same vector without issue. All incoming datetimes will be
+#' interpreted in the specified timezone.
+#'
+#' If \code{datetime} is a \code{POSIXct} it will be returned unmodified, and
+#' formats not recognized will be returned as \code{NA}.
+#'
+#' @section PWFSLSmoke Conventions:
+#' Within the PWFSLSmoke package, datetimes not in \code{POSIXct} format are
+#' represented as decimal values with no separation (ex: 20181012,
+#' 20181012130900), either as numerics or strings.
+#'
+#' @section Implementation:
+#' \code{parseDatetime} is essentially a wrapper around
+#' \code{\link[packagename]{functioname}}, handling which formats we want to
+#' account for.
+#'
+#' @param datetime vector of character or integer datetimes in YYYYMMDD[HHMMSS]
+#'   format (or POSIXct).
+#' @param timezone Olson timezone at the location of interest (default "UTC").
+#' @param expectAll Logical value determining if the function should fail if
+#'   any elements fail to parse (default \code{FALSE}).
+#'
 #' @return POSIXct datetimes.
+#'
+#' @seealso \code{\link[lubridate]{parse_date_time}} for implementation details.
+#'
 #' @examples
-#' starttime <- parseDatetime(2015080718,timezone = "America/Los_Angeles")
+#' starttime <- parseDatetime(2015080718, timezone = "America/Los_Angeles")
 
-parseDatetime <- function(datetime, timezone='UTC') {
-  
-  if ( 'POSIXct' %in% class(datetime) ) {
-    
-    # No need to parse
+parseDatetime <- function(datetime, timezone= "UTC", expectAll = FALSE) {
 
-  } else {
-    
-    datetime <- as.character(datetime)
-    length <- stringr::str_length(datetime)[1]
-    
-    if (length == 8) {
-      datetime <- lubridate::ymd(datetime, tz=timezone)
-    } else if (length == 10) {
-      datetime <- lubridate::ymd_h(datetime, tz=timezone)
-    } else if (length == 12) {
-      datetime <- lubridate::ymd_hm(datetime, tz=timezone)
-    } else if (length == 14) {
-      datetime <- lubridate::ymd_hms(datetime, tz=timezone)
-    } else {
-      stop(paste0('Datetime "',datetime,'" cannot be parsed.'))
-    }
-    
+  orders <- c("Ymd", "YmdH", "YmdHM", "YmdHMS")
+  parsedDatetime <- lubridate::parse_date_time(datetime, orders, tz = timezone)
+
+  if (all(is.na(datetime))) {
+    stop(paste0("No datetimes could be parsed."))
   }
 
-  # guarantee that we return a POSIXct time
-  return(as.POSIXct(datetime))
-  
+  # If there already exist NAs in datetime, we don't want to accidently fail
+  # if all non-NA values were parsed
+  if (expectAll && which(is.na(datetime)) != which(is.na(parsedDatetime))) {
+
+    stop(paste0(
+      sum(is.na(datetime)), " datetimes could not be parsed",
+      " (at indices: ", which(is.na(datetime)), ")."
+    ))
+
+  }
+
+  return(datetime)
 }
