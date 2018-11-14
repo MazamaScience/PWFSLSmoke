0c5736177f7d8f6532bc34931eb55cd26779150c
Spencer Pease <spencer.p.816@gmail.com>
2018-11-13 18:41:02 -0800

    Start work on determining latency (WIP)
    References issue #25

--- R/monitor_getCurrentStatus.R
+++ R/monitor_getCurrentStatus.R
@@ -26,4 +26,46 @@ monitor_getCurrentStatus <- function(ws_monitor,
   }
 
 
+# Calculate data latency --------------------------------------------------
+
+  ## TODO: how are different timezones handled when compared against endTime
+  #  and processTime?
+
+  processTime <- lubridate::now("UTC")
+
+  latencyTbl <- ws_monitor %>%
+    monitor_toTidy() %>%
+    group_by(.data$monitorID) %>%
+    filter(!is.na(.data$pm25)) %>%
+    summarize(lastUpdate = max(.data$datetime)) %>%
+    ungroup(.data$monitorID) %>%
+    mutate(latency = endTimeInclusive - .data$lastUpdate) %>%
+    filter(.data$latency <= lubridate::dhours(maxLatency))
+
+
+# Add events --------------------------------------------------------------
+
+  ## Events:
+  #
+  #  * USG6 -- NowCast level increased to Unhealthy for Sensitive Groups in the
+  #            last 6 hours
+  #  * U6   -- NowCast level increased to Unhealthy in the last 6 hours
+  #  * VU6  -- NowCast level increased to Very Unhealthy in the last 6 hours
+  #  * HAZ6 -- NowCast level increased to Hazardous in the last 6 hours
+  #  * MOD6 -- NowCast level decreased to Moderate or Good in the last 6 hours
+  #  * NR6  -- Monitor not reporting for more than 6 hours
+  #  * MAL6 -- Monitor malfunctioning the last 6 hours
+  #  * NEW6 -- New monitor reporting in the last 6 hours
+
+}
+
+
+# DEBUGGING
+if (FALSE) {
+
+  ws_monitor <- monitor_loadLatest() %>%
+    monitor_subset(stateCodes = "WA")
+  endTime <-  lubridate::now("UTC")
+  maxLatency <-  6
+
 }
