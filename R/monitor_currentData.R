103d2fa36cdc7fed9b28952fc187c84dba30bab0
Spencer Pease <spencer.p.816@gmail.com>
2018-12-12 19:45:27 -0800

    Fix bug in `monitor_currentData()`
    Now able to correctly build montioring site urls.

--- R/monitor_currentData.R
+++ R/monitor_currentData.R
@@ -178,6 +178,7 @@ monitor_currentData <- function(ws_monitor,
   currentData$yesterday_PM2.5_24hr <- as.numeric(NA)
 
 
+
   # Values that must be calculated per-monitoring-site
   for (monitorID in currentData$monitorID) {
 
