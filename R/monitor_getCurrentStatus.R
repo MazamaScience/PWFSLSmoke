121d31d827904820e1ffaa508ee8b104e3578bd1
Spencer Pease <spencer.p.816@gmail.com>
2018-12-18 17:14:34 -0800

    Add events to `monitor_getCurrentStatus()`
    References issue #25

--- R/monitor_getCurrentStatus.R
+++ R/monitor_getCurrentStatus.R
@@ -122,6 +122,21 @@ monitor_getCurrentStatus <- function(ws_monitor,
   #  * MAL6 -- Monitor malfunctioning the last 6 hours
   #  * NEW6 -- New monitor reporting in the last 6 hours
 
+  eventData <- currentStatus %>%
+    left_join(summaryData, by = "monitorID") %>%
+    mutate(
+      `USG6` = .levelChnage(.data, 3, 6, "increase"),
+      `U6` = .levelChnage(.data, 4, 6, "increase"),
+      `VU6` = .levelChnage(.data, 5, 6, "increase"),
+      `HAZ6` = .levelChnage(.data, 6, 6, "increase"),
+      `MOD6` = .levelChnage(.data, 2, 6, "decrease") | .levelChnage(.data, 1, 6, "decrease")
+    ) %>%
+    list(
+      .isNotReporting(ws_data, 6, endTimeInclusive, "NR6"),
+      .isNewReporting(ws_data, 6, 48, endTimeInclusive, "NEW6")
+    ) %>%
+    purrr::reduce(left_join, by = "monitorID") %>%
+    mutate(`MAL6` = NA)
 
 
 # Return output -----------------------------------------------------------
@@ -129,8 +144,7 @@ monitor_getCurrentStatus <- function(ws_monitor,
   # Note: option to not append meta info?
 
   outputData <- ws_meta %>%
-    left_join(currentStatus, by = "monitorID") %>%
-    left_join(summaryData, by = "monitorID")
+    left_join(eventData, by = "monitorID")
 
   return(outputData)
 
@@ -233,5 +247,57 @@ if (FALSE) {
     .bincode(AQI$breaks_24, include.lowest = TRUE)
 
   return(tibble(`monitorID` = names(timeIndices), !!qColTitle := levels))
+
+}
+
+.levelChnage <- function(data, level, n, direction) {
+
+  if (direction == "increase") {
+    result <-
+      (data$last_nowcastLevel >= level) &
+      (data$previous_nowcastLevel < level)
+  } else if (direction == "decrease") {
+    result <-
+      (data$previous_nowcastLevel > level) &
+      (data$last_nowcastLevel <= level)
+  } else {
+    stop("'direction' must be either 'increase' or 'decrease'.")
+  }
+
+  return(result & (data$last_latency + data$previous_latency <= n))
+
+}
+
+.isNotReporting <- function(data, n, endTimeUTC, colTitle) {
+
+  qColTitle <- quo_name(enquo(colTitle))
+
+  startTimeInclusive <- endTimeUTC %>%
+    magrittr::subtract(lubridate::dhours(n - 1))
+
+  result <- data %>%
+    filter(.data$datetime >= startTimeInclusive) %>%
+    select(-.data$datetime) %>%
+    summarise_all(~all(is.na(.))) %>%
+    tidyr::gather("monitorID", !!qColTitle)
+
+  return(result)
+}
+
+.isNewReporting <- function(data, n, buffer, endTimeUTC, colTitle) {
+
+  qColTitle <- quo_name(enquo(colTitle))
+
+  bufferEndTime <- endTimeUTC - lubridate::dhours(n)
+
+  recentReport <- .isNotReporting(data, n, endTimeUTC, "recent")
+  bufferReport <- .isNotReporting(data, buffer, bufferEndTime, "buffer")
+
+  result <-
+    left_join(recentReport, bufferReport, by = "monitorID") %>%
+    mutate(!!colTitle := .data$buffer & !.data$recent) %>%
+    select(-.data$buffer, -.data$recent)
+
+  return(result)
 
 }
