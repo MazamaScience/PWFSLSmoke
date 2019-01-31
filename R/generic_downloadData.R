1ea7f276174fca2b8fc82eb0877d784f6149b8bb
Spencer Pease <spencer.p.816@gmail.com>
2019-01-31 14:56:40 -0800

    Rename `generic_downloadData()` parameters
    `filePath` is better representative of the types of input
    `generic_downladData()` can accept.
    
    References issue #50

--- R/generic_downloadData.R
+++ R/generic_downloadData.R
@@ -1,13 +1,13 @@
-generic_downloadData <- function(fileURL) {
+generic_downloadData <- function(filePath) {
 
   logger.info(
     paste0(
       "Reading file from location:\n",
-      stringr::str_trunc(fileURL, 80, side = "right")
+      stringr::str_trunc(filePath, 80, side = "right")
     )
   )
 
-  fileString <- readr::read_file(fileURL)
+  fileString <- readr::read_file(filePath)
 
   return(fileString)
 
