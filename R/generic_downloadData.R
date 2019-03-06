128e67eada00f96b990a304d9535029f2d319d00
Spencer Pease <spencer.p.816@gmail.com>
2019-03-05 16:27:05 -0800

    Remove path truncation
    References issue #50

--- R/generic_downloadData.R
+++ R/generic_downloadData.R
@@ -24,12 +24,7 @@
 #' }
 generic_downloadData <- function(filePath) {
 
-  logger.info(
-    paste0(
-      "Reading file from location:\n",
-      stringr::str_trunc(filePath, 80, side = "right")
-    )
-  )
+  logger.info(paste0("Reading file from location:\n", filePath))
 
   fileString <- readr::read_file(filePath)
 
