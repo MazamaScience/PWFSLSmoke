1867c7b1202e342dfad9700ceddb1cab3444b1b8
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2020-11-24 16:22:09 -0800

    Katy Warner request

--- local_jon/Katy_Warner_request.R
+++ local_jon/Katy_Warner_request.R
@@ -14,24 +14,83 @@
 #
 
 library(PWFSLSmoke)
+library(PWFSLSmokePlots)
 
+# Get all California monitors for the time period
 CA_monitor <-
   monitor_loadAnnual(2020) %>%
   monitor_subset(stateCodes = c("CA")) %>%
   monitor_subset(tlim = c(20200901, 20201115)) %>%
   monitor_trim()
 
-# monitor_leaflet(CA_monitor)
+monitor_leaflet(CA_monitor)
 
-YosemiteVillage <-
-  ws_monitor %>%
+# Isolate the Yosemite Village monitor
+Yosemite_Village <-
+  CA_monitor %>%
   monitor_subset(monitorID = "060431001_01")
 
+# Find all monitors within 20 km of Yosemite Village
 ws_monitor <-
   CA_monitor %>%
   monitor_subsetByDistance(
-    longitude = YosemiteVillage$meta$longitude,
-    latitude = YosemiteVillage$meta$latitude,
-    radius = 50
+    longitude = Yosemite_Village$meta$longitude,
+    latitude = Yosemite_Village$meta$latitude,
+    radius = 30
   )
 
+monitor_leaflet(ws_monitor)
+
+# Create daily-hourly barplot
+dailyHourlyBarplot(ws_monitor)
+
+# ==============================================================================
+
+# So, what I really need for this report are the ones labeled Yosemite Village -
+# Visitor Center, El Portal - 5518 Foresta Rd, Yosemite National Park -7799
+# Chilnualna Rd, and the stitched together ones of Long Barn - 9075 Tuolumne
+# Meadows Lodge Rd with Long Barn - 9073 Tuolumne Meadows Lodge Rd.  And it would
+# be good to put the Groveland - Hodgdon Meadow Access Rd at the bottom, since
+# it clearly showed some impacts (but wasn't on the outlook.)
+
+El_Portal <-
+  CA_monitor %>%
+  monitor_subset(monitorID = "lon_.119.784_lat_37.675_apcd.1011")
+
+Chilnualna_Rd <-
+  CA_monitor %>%
+  monitor_subset(monitorID = "lon_.119.658_lat_37.540_apcd.1020")
+
+Groveland <-
+  CA_monitor %>%
+  monitor_subset(monitorID = "lon_.119.860_lat_37.797_apcd.1019")
+
+# Combine the two Long Barn timeseries
+Long_Barn_siteName <- "Long Barn - Tuolomne Meadows Lodge Rd"
+
+Long_Barn_9705 <-
+  CA_monitor %>%
+  monitor_subset(monitorID = "lon_.119.343_lat_37.877_wrcc.smy1")
+Long_Barn_9705$meta$siteName <- Long_Barn_siteName
+Long_Barn_9705$meta$monitorID <- "LongBarnFakeMonitorID"
+names(Long_Barn_9705$data) <- c("datetime", "LongBarnFakeMonitorID")
+
+Long_Barn_9703 <-
+  CA_monitor %>%
+  monitor_subset(monitorID = "lon_.119.343_lat_37.877_apcd.1019")
+Long_Barn_9703$meta$siteName <- Long_Barn_siteName
+Long_Barn_9703$meta$monitorID <- "LongBarnFakeMonitorID"
+names(Long_Barn_9703$data) <- c("datetime", "LongBarnFakeMonitorID")
+
+# Join the two Long Barn timeseries
+Tuolomne_Meadows <- monitor_join(Long_Barn_9705, Long_Barn_9703)
+
+
+# Combine monitors of interest
+my_monitors <- monitor_combine(list(
+  Yosemite_Village, El_Portal, Chilnualna_Rd,
+  Tuolomne_Meadows, Groveland
+))
+
+# New plot
+dailyHourlyBarplot(my_monitors)
